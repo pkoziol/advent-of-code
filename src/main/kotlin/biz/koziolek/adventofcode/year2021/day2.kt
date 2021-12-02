e47b7bb605457a8240920b02793c2a38aee9129a
koziolek <github@koziolek.biz>
2021-12-02 11:41:34 +0100

    Refactor 2021-12-02

--- src/main/kotlin/biz/koziolek/adventofcode/year2021/day2.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2021/day2.kt
@@ -13,44 +13,39 @@ fun main() {
     println("Answer 2: ${positionWithAim.horizontal * positionWithAim.depth}")
 }
 
-data class Position(val horizontal: Int,
-                    val depth: Int)
+data class Position(val horizontal: Int, val depth: Int)
 
-data class PositionAndAim(val position: Position,
-                          val aim: Int)
+data class PositionAndAim(val position: Position, val aim: Int)
+
+data class Command(val direction: String, val value: Int)
 
 fun calculatePosition(lines: Sequence<String>): Position =
-        lines
-                .map { it.split(" ", limit = 2) }
-                .filter { it.size == 2 }
-                .fold(Position(0, 0)) { acc, splitCommand ->
-                    val direction = splitCommand[0]
-                    val value = splitCommand[1].toInt()
-
-                    when (direction) {
-                        "forward" -> acc.copy(horizontal = acc.horizontal + value)
-                        "down" -> acc.copy(depth = acc.depth + value)
-                        "up" -> acc.copy(depth = acc.depth - value)
-                        else -> throw IllegalArgumentException("Unknown direction: $direction")
+        parseCommands(lines)
+                .fold(Position(0, 0)) { acc, command ->
+                    when (command.direction) {
+                        "forward" -> acc.copy(horizontal = acc.horizontal + command.value)
+                        "down" -> acc.copy(depth = acc.depth + command.value)
+                        "up" -> acc.copy(depth = acc.depth - command.value)
+                        else -> throw IllegalArgumentException("Unknown direction: ${command.direction}")
                     }
                 }
 
 fun calculatePositionWithAim(lines: Sequence<String>): Position =
-        lines
-                .map { it.split(" ", limit = 2) }
-                .filter { it.size == 2 }
-                .fold(PositionAndAim(Position(0, 0), 0)) { acc, splitCommand ->
-                    val direction = splitCommand[0]
-                    val value = splitCommand[1].toInt()
-
-                    when (direction) {
+        parseCommands(lines)
+                .fold(PositionAndAim(Position(0, 0), 0)) { acc, command ->
+                    when (command.direction) {
                         "forward" -> acc.copy(position = acc.position.copy(
-                                horizontal = acc.position.horizontal + value,
-                                depth = acc.position.depth + acc.aim  * value
+                                horizontal = acc.position.horizontal + command.value,
+                                depth = acc.position.depth + acc.aim  * command.value
                         ))
-                        "down" -> acc.copy(aim = acc.aim + value)
-                        "up" -> acc.copy(aim = acc.aim - value)
-                        else -> throw IllegalArgumentException("Unknown direction: $direction")
+                        "down" -> acc.copy(aim = acc.aim + command.value)
+                        "up" -> acc.copy(aim = acc.aim - command.value)
+                        else -> throw IllegalArgumentException("Unknown direction: ${command.direction}")
                     }
                 }
                 .position
+
+private fun parseCommands(lines: Sequence<String>) = lines
+        .map { it.split(" ", limit = 2) }
+        .filter { it.size == 2 }
+        .map { Command(direction = it[0], value = it[1].toInt()) }
