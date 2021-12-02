cf6f180f3f0ac35ca511893e6a6874a8ce0f8e17
koziolek <github@koziolek.biz>
2021-12-02 10:02:03 +0100

    Solution for 2021-12-02, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2021/day2.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2021/day2.kt
@@ -6,21 +6,51 @@ fun main() {
     val inputFile = File("src/main/resources/year2021/day2/input")
     val position = calculatePosition(inputFile.bufferedReader().lineSequence())
     println("Position: $position")
-    println("Answer: ${position.first * position.second}")
+    println("Answer 1: ${position.horizontal * position.depth}")
+
+    val positionWithAim = calculatePositionWithAim(inputFile.bufferedReader().lineSequence())
+    println("Position with aim: $positionWithAim")
+    println("Answer 2: ${positionWithAim.horizontal * positionWithAim.depth}")
 }
 
-fun calculatePosition(lines: Sequence<String>): Pair<Int, Int> =
+data class Position(val horizontal: Int,
+                    val depth: Int)
+
+data class PositionAndAim(val position: Position,
+                          val aim: Int)
+
+fun calculatePosition(lines: Sequence<String>): Position =
         lines
                 .map { it.split(" ", limit = 2) }
                 .filter { it.size == 2 }
-                .fold(Pair(0, 0)) { acc, splitCommand ->
+                .fold(Position(0, 0)) { acc, splitCommand ->
                     val direction = splitCommand[0]
                     val value = splitCommand[1].toInt()
 
                     when (direction) {
-                        "forward" -> acc.copy(first = acc.first + value)
-                        "down" -> acc.copy(second = acc.second + value)
-                        "up" -> acc.copy(second = acc.second - value)
+                        "forward" -> acc.copy(horizontal = acc.horizontal + value)
+                        "down" -> acc.copy(depth = acc.depth + value)
+                        "up" -> acc.copy(depth = acc.depth - value)
                         else -> throw IllegalArgumentException("Unknown direction: $direction")
                     }
                 }
+
+fun calculatePositionWithAim(lines: Sequence<String>): Position =
+        lines
+                .map { it.split(" ", limit = 2) }
+                .filter { it.size == 2 }
+                .fold(PositionAndAim(Position(0, 0), 0)) { acc, splitCommand ->
+                    val direction = splitCommand[0]
+                    val value = splitCommand[1].toInt()
+
+                    when (direction) {
+                        "forward" -> acc.copy(position = acc.position.copy(
+                                horizontal = acc.position.horizontal + value,
+                                depth = acc.position.depth + acc.aim  * value
+                        ))
+                        "down" -> acc.copy(aim = acc.aim + value)
+                        "up" -> acc.copy(aim = acc.aim - value)
+                        else -> throw IllegalArgumentException("Unknown direction: $direction")
+                    }
+                }
+                .position
--- src/test/kotlin/biz/koziolek/adventofcode/year2021/Day2KtTest.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2021/Day2KtTest.kt
@@ -18,15 +18,32 @@ internal class Day2KtTest {
     @Test
     fun testSampleInput() {
         val position = calculatePosition(sampleInput.splitToSequence("\n"))
-        assertEquals(15, position.first)
-        assertEquals(10, position.second)
+        assertEquals(15, position.horizontal)
+        assertEquals(10, position.depth)
     }
+
+    @Test
+    fun testSampleInputWithAim() {
+        val position = calculatePositionWithAim(sampleInput.splitToSequence("\n"))
+        assertEquals(15, position.horizontal)
+        assertEquals(60, position.depth)
+    }
+
     @Test
     fun testFullInput() {
         val position = File("src/main/resources/year2021/day2/input").useLines {
             calculatePosition(it)
         }
-        assertEquals(1991, position.first)
-        assertEquals(911, position.second)
+        assertEquals(1991, position.horizontal)
+        assertEquals(911, position.depth)
+    }
+
+    @Test
+    fun testFullInputWithAim() {
+        val position = File("src/main/resources/year2021/day2/input").useLines {
+            calculatePositionWithAim(it)
+        }
+        assertEquals(1991, position.horizontal)
+        assertEquals(984716, position.depth)
     }
 }
