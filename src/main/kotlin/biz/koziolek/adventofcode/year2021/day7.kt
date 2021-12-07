8277c426fcb2aac889ed4c7cb23cd25983486b45
koziolek <github@koziolek.biz>
2021-12-07 09:17:15 +0100

    Refactoring 2021-12-07, part 1

--- src/main/kotlin/biz/koziolek/adventofcode/year2021/day7.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2021/day7.kt
@@ -6,20 +6,20 @@ import kotlin.math.abs
 fun main() {
     val inputFile = File("src/main/resources/year2021/day7/input")
     val line = inputFile.bufferedReader().readLines().first()
-    val (position, cost) = findCheapestPosition(line)
+    val (position, cost) = findCheapestPosition(line, ::calculateLinearCost)
 
     println("Cheapest position is $position with cost: $cost")
 }
 
-fun findCheapestPosition(line: String): Pair<Int, Int> {
+fun findCheapestPosition(line: String, costFunction: (Int, Int) -> Int): Pair<Int, Int> {
     val initialPositions = line.split(',')
             .map { it.toInt() }
     
     return (0..initialPositions.maxOf { it })
-            .map { Pair(it, calculateTotalCost(initialPositions, it)) }
+            .map { dst -> Pair(dst, initialPositions.sumOf { src -> costFunction(src, dst) }) }
             .minByOrNull { it.second }
             ?: throw IllegalArgumentException("Could not find cheapest position")
 }
 
-fun calculateTotalCost(sourcePositions: List<Int>, destination: Int): Int =
-        sourcePositions.map { abs(it - destination) }.sum()
+fun calculateLinearCost(source: Int, destination: Int): Int =
+        abs(source - destination)
--- src/test/kotlin/biz/koziolek/adventofcode/year2021/Day7Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2021/Day7Test.kt
@@ -9,8 +9,22 @@ internal class Day7Test {
     private val sampleInput = "16,1,2,0,4,2,7,1,2,14"
 
     @Test
+    fun testCalculateLinearCost() {
+        assertEquals(14, calculateLinearCost(16, 2))
+        assertEquals(1, calculateLinearCost(1, 2))
+        assertEquals(0, calculateLinearCost(2, 2))
+        assertEquals(2, calculateLinearCost(0, 2))
+        assertEquals(2, calculateLinearCost(4, 2))
+        assertEquals(0, calculateLinearCost(2, 2))
+        assertEquals(5, calculateLinearCost(7, 2))
+        assertEquals(1, calculateLinearCost(1, 2))
+        assertEquals(0, calculateLinearCost(2, 2))
+        assertEquals(12, calculateLinearCost(14, 2))
+    }
+
+    @Test
     fun testFindCheapestPositionSample() {
-        val (position, cost) = findCheapestPosition(sampleInput)
+        val (position, cost) = findCheapestPosition(sampleInput, ::calculateLinearCost)
         assertEquals(2, position)
         assertEquals(37, cost)
     }
@@ -18,7 +32,7 @@ internal class Day7Test {
     @Test
     fun testAnswerPart1() {
         val fullInput = File("src/main/resources/year2021/day7/input").readLines().first()
-        val (position, cost) = findCheapestPosition(fullInput)
+        val (position, cost) = findCheapestPosition(fullInput, ::calculateLinearCost)
         assertEquals(324, position)
         assertEquals(344535, cost)
     }
