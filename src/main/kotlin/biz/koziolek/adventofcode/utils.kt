cb8f6235fc480bd4d07d08a8bb8134afb1d52fba
koziolek <github@koziolek.biz>
2021-12-11 17:48:59 +0100

    Solution for 2021-12-11, part 1

--- src/main/kotlin/biz/koziolek/adventofcode/utils.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/utils.kt
@@ -18,3 +18,26 @@ fun <T> visitAll(start: T, visitor: (T) -> Iterable<T>) {
         current = toVisit.poll()
     }
 }
+
+private val ESC = Char(27)
+
+enum class AsciiColor(val code: Int) {
+    BLACK(30),
+    RED(31),
+    GREEN(32),
+    YELLOW(33),
+    BLUE(34),
+    MAGENTA(35),
+    CYAN(36),
+    WHITE(37),
+    BRIGHT_BLACK(90),
+    BRIGHT_RED(91),
+    BRIGHT_GREEN(92),
+    BRIGHT_YELLOW(93),
+    BRIGHT_BLUE(94),
+    BRIGHT_MAGENTA(95),
+    BRIGHT_CYAN(96),
+    BRIGHT_WHITE(97);
+
+    fun format(obj: Any?) = "$ESC[${code}m$obj$ESC[0m"
+}
