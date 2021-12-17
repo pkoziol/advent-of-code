ec68e368475c079138263f93e0065e7be207834c
koziolek <github@koziolek.biz>
2021-12-17 11:19:04 +0100

    Solution for 2021-12-17, part 1

---
+++ src/main/kotlin/biz/koziolek/adventofcode/year2021/day17/day17.kt
@@ -0,0 +1,112 @@
+package biz.koziolek.adventofcode.year2021.day17
+
+import biz.koziolek.adventofcode.*
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    val inputFile = findInput(object {})
+    val line = inputFile.bufferedReader().readLine()
+    val targetArea = parseTargetArea(line)
+    val (bestVelocity, maxHeight) = findVelocityWithMostStyle(targetArea)
+
+    println("Best velocity is: $bestVelocity that gives max height: $maxHeight")
+}
+
+fun parseTargetArea(line: String): Pair<Coord, Coord> =
+    Regex("target area: x=(-?[0-9]+)\\.\\.(-?[0-9]+), y=(-?[0-9]+)\\.\\.(-?[0-9]+)")
+        .find(line)
+        ?.let {
+            val x1 = it.groupValues[1].toInt()
+            val x2 = it.groupValues[2].toInt()
+            val y1 = it.groupValues[3].toInt()
+            val y2 = it.groupValues[4].toInt()
+
+            Coord(min(x1, x2), max(y1, y2)) to Coord(max(x1, x2), min(y1, y2))
+        }
+        ?: throw IllegalArgumentException("Could not parse target area: $line")
+
+fun findVelocityWithMostStyle(targetArea: Pair<Coord, Coord>): Pair<Coord, Int> =
+    generateVelocities(0, 1000, 0, 1000)
+        .map { velocity -> velocity to calculateTrajectory(velocity, targetArea) }
+        .filter { (_, trajectory) -> trajectory.last() in targetArea }
+        .map { (velocity, trajectory) -> velocity to trajectory.maxOf { it.y } }
+        .sortedByDescending { (_, maxHeight) -> maxHeight }
+        .first()
+
+private fun generateVelocities(minX: Int, maxX: Int, minY: Int, maxY: Int): Sequence<Coord> =
+    sequence {
+        for (x in minX..maxX) {
+            for (y in minY..maxY) {
+                yield(Coord(x, y))
+            }
+        }
+    }
+
+fun calculateTrajectory(velocity: Coord, targetArea: Pair<Coord, Coord>, limit: Int = 1_000_000): List<Coord> =
+    generateSequence(Triple(Coord(0, 0), velocity, 0)) { (coord, velocity, iteration) ->
+        if (iteration > limit) {
+            throw IllegalStateException("Trajectory calculation interrupted by limit")
+        } else if (coord in targetArea) {
+            null
+        } else {
+            val newCoord = coord + velocity
+            val newVelocity = Coord(
+                x = when {
+                    velocity.x > 0 -> velocity.x - 1
+                    velocity.x < 0 -> velocity.x + 1
+                    else -> velocity.x
+                },
+                y = velocity.y - 1
+            )
+
+            if (velocity.y < 0 && newCoord.y < targetArea.second.y
+                || velocity.x > 0 && newCoord.x > targetArea.second.x) {
+                null
+            } else {
+                Triple(newCoord, newVelocity, iteration + 1)
+            }
+        }
+    }
+        .map { it.first }
+        .toList()
+
+fun visualizeTrajectory(trajectory: List<Coord>, targetArea: Pair<Coord, Coord>): String =
+    buildString {
+        val allX = trajectory.map { it.x }.plus(targetArea.first.x).plus(targetArea.second.x)
+        val allY = trajectory.map { it.y }.plus(targetArea.first.y).plus(targetArea.second.y)
+        val minX = allX.minOrNull() ?: 0
+        val maxX = allX.maxOrNull() ?: 0
+        val minY = allY.minOrNull() ?: 0
+        val maxY = allY.maxOrNull() ?: 0
+        
+        for (y in maxY downTo minY) {
+            for (x in minX..maxX) {
+                val coord = Coord(x, y)
+                val trajectoryIndex = trajectory.indexOf(coord)
+                
+                if (trajectoryIndex == 0) {
+                    append('S')
+                } else if (trajectoryIndex > 0) {
+                    append('#')
+                } else if (coord in targetArea) {
+                    append('T')
+                } else {
+                    append('.')
+                }
+            }
+
+            if (y != minY) {
+                append('\n')
+            }
+        }
+    }
+
+private operator fun Pair<Coord, Coord>.contains(coord: Coord): Boolean {
+    val minX = min(first.x, second.x)
+    val maxX = max(first.x, second.x)
+    val minY = min(first.y, second.y)
+    val maxY = max(first.y, second.y)
+
+    return coord.x in minX..maxX && coord.y in minY..maxY
+}
---
+++ src/test/kotlin/biz/koziolek/adventofcode/year2021/day17/Day17Test.kt
@@ -0,0 +1,153 @@
+package biz.koziolek.adventofcode.year2021.day17
+
+import biz.koziolek.adventofcode.Coord
+import biz.koziolek.adventofcode.findInput
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day17Test {
+
+    private val sampleInput = "target area: x=20..30, y=-10..-5"
+
+    @Test
+    fun testParseTargetAreaSample() {
+        val targetArea = parseTargetArea(sampleInput)
+        assertEquals(Coord(20, -5), targetArea.first)
+        assertEquals(Coord(30, -10), targetArea.second)
+    }
+
+    @Test
+    fun testParseTargetAreaFullInput() {
+        val fullInput = findInput(object {}).readLines()
+        val targetArea = parseTargetArea(fullInput.single())
+        assertEquals(Coord(265, -58), targetArea.first)
+        assertEquals(Coord(287, -103), targetArea.second)
+    }
+
+    @Test
+    fun testSampleTrajectory1() {
+        val targetArea = Coord(20, -5) to Coord(30, -10)
+        val velocity = Coord(7, 2)
+
+        val trajectory = calculateTrajectory(velocity, targetArea)
+        assertEquals(8, trajectory.size)
+        assertEquals(Coord(0, 0), trajectory[0])
+        assertEquals(Coord(7, 2), trajectory[1])
+        assertEquals(Coord(13, 3), trajectory[2])
+        assertEquals(Coord(18, 3), trajectory[3])
+        assertEquals(Coord(22, 2), trajectory[4])
+        assertEquals(Coord(25, 0), trajectory[5])
+        assertEquals(Coord(27, -3), trajectory[6])
+        assertEquals(Coord(28, -7), trajectory[7])
+
+        val expectedVisualization = """
+            .............#....#............
+            .......#..............#........
+            ...............................
+            S........................#.....
+            ...............................
+            ...............................
+            ...........................#...
+            ...............................
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTT#TT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+        """.trimIndent()
+        assertEquals(expectedVisualization, visualizeTrajectory(trajectory, targetArea))
+    }
+
+    @Test
+    fun testSampleTrajectory2() {
+        val targetArea = Coord(20, -5) to Coord(30, -10)
+        val velocity = Coord(6, 3)
+
+        val trajectory = calculateTrajectory(velocity, targetArea)
+
+        val expectedVisualization = """
+            ...............#..#............
+            ...........#........#..........
+            ...............................
+            ......#..............#.........
+            ...............................
+            ...............................
+            S....................#.........
+            ...............................
+            ...............................
+            ...............................
+            .....................#.........
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................T#TTTTTTTTT
+            ....................TTTTTTTTTTT
+        """.trimIndent()
+        assertEquals(expectedVisualization, visualizeTrajectory(trajectory, targetArea))
+    }
+
+    @Test
+    fun testSampleTrajectory3() {
+        val targetArea = Coord(20, -5) to Coord(30, -10)
+        val velocity = Coord(9, 0)
+
+        val trajectory = calculateTrajectory(velocity, targetArea)
+
+        val expectedVisualization = """
+            S........#.....................
+            .................#.............
+            ...............................
+            ........................#......
+            ...............................
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTT#
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+        """.trimIndent()
+        assertEquals(expectedVisualization, visualizeTrajectory(trajectory, targetArea))
+    }
+
+    @Test
+    fun testSampleTrajectory4() {
+        val targetArea = Coord(20, -5) to Coord(30, -10)
+        val velocity = Coord(17, -4)
+
+        val trajectory = calculateTrajectory(velocity, targetArea)
+
+        val expectedVisualization = """
+            S..............................
+            ...............................
+            ...............................
+            ...............................
+            .................#.............
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+            ....................TTTTTTTTTTT
+        """.trimIndent()
+        assertEquals(expectedVisualization, visualizeTrajectory(trajectory, targetArea))
+    }
+
+    @Test
+    fun testFindVelocityWithMostStyle() {
+        val targetArea = Coord(20, -5) to Coord(30, -10)
+        val bestVelocityAndMaxHeight = findVelocityWithMostStyle(targetArea)
+
+        assertEquals(Coord(6, 9) to 45, bestVelocityAndMaxHeight)
+    }
+
+    @Test
+    fun testAnswer1() {
+        val fullInput = findInput(object {}).readLines()
+        val targetArea = parseTargetArea(fullInput.single())
+        val bestVelocityAndMaxHeight = findVelocityWithMostStyle(targetArea)
+
+        assertEquals(Coord(23, 102) to 5253, bestVelocityAndMaxHeight)
+    }
+}
