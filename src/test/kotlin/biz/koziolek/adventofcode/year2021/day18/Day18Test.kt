c83cae1b02a55f99b81b809cef60c1d7b4659443
koziolek <github@koziolek.biz>
2021-12-18 22:54:09 +0100

    Solution for 2021-12-18, part 2

--- src/test/kotlin/biz/koziolek/adventofcode/year2021/day18/Day18Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2021/day18/Day18Test.kt
@@ -358,4 +358,34 @@ internal class Day18Test {
         val sum = addAndReduceAll(numbers)
         assertEquals(4176, sum.magnitude)
     }
+
+    @Test
+    fun testFindMaxPossibleMagnitudeOfTwo() {
+        val lines = """
+            [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+            [[[5,[2,8]],4],[5,[[9,9],0]]]
+            [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+            [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+            [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+            [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+            [[[[5,4],[7,7]],8],[[8,3],8]]
+            [[9,3],[[9,9],[6,[4,9]]]]
+            [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+            [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+        """.trimIndent().split("\n")
+        val numbers = parseSnailfishNumbers(lines)
+        val (firstNum, secondNum, maxMagnitude) = findMaxPossibleMagnitudeOfTwo(numbers)
+        assertEquals("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]", firstNum.asText())
+        assertEquals("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]", secondNum.asText())
+        assertEquals(3993, maxMagnitude)
+    }
+
+    @Test
+    fun testAnswer2() {
+        val fullInput = findInput(object {}).readLines()
+        val numbers = parseSnailfishNumbers(fullInput)
+        val (_, _, maxMagnitude) = findMaxPossibleMagnitudeOfTwo(numbers)
+        assertEquals(4633, maxMagnitude)
+    }
 }
+
