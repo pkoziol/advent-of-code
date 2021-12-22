136c54681614486e16b30938f94afb67d8b0c70d
koziolek <github@koziolek.biz>
2021-12-22 10:18:17 +0100

    Refactor 2021-12-22, part 1 a little

--- src/main/kotlin/biz/koziolek/adventofcode/year2021/day22/day22.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2021/day22/day22.kt
@@ -10,7 +10,10 @@ fun main() {
     println("On cubes: $onCubes")
 }
 
-data class RebootStep(val turnOn: Boolean, val from: Coord3d, val to: Coord3d) {
+data class RebootStep(val turnOn: Boolean, val cuboid: Cuboid)
+
+data class Cuboid(val from: Coord3d, val to: Coord3d) {
+
     fun getCubes(): Sequence<Coord3d> =
         (from.x..to.x).asSequence().flatMap { x ->
             (from.y..to.y).asSequence().flatMap { y ->
@@ -19,6 +22,16 @@ data class RebootStep(val turnOn: Boolean, val from: Coord3d, val to: Coord3d) {
                 }
             }
         }
+
+    fun intersects(other: Cuboid) =
+        this.from.x <= other.to.x && other.from.x <= this.to.x
+                && this.from.y <= other.to.y && other.from.y <= this.to.y
+                && this.from.z <= other.to.z && other.from.z <= this.to.z
+
+    operator fun contains(coord3d: Coord3d) =
+        from.x <= coord3d.x && coord3d.x <= to.x
+                && from.y <= coord3d.y && coord3d.y <= to.y
+                && from.z <= coord3d.z && coord3d.z <= to.z
 }
 
 fun parseReactorRebootSteps(lines: List<String>): List<RebootStep> =
@@ -28,32 +41,34 @@ fun parseReactorRebootSteps(lines: List<String>): List<RebootStep> =
             ?.let { result ->
                 RebootStep(
                     turnOn = result.groupValues[1] == "on",
-                    from = Coord3d(
-                        x = result.groupValues[2].toInt(),
-                        y = result.groupValues[4].toInt(),
-                        z = result.groupValues[6].toInt(),
-                    ),
-                    to = Coord3d(
-                        x = result.groupValues[3].toInt(),
-                        y = result.groupValues[5].toInt(),
-                        z = result.groupValues[7].toInt(),
+                    cuboid = Cuboid(
+                        from = Coord3d(
+                            x = result.groupValues[2].toInt(),
+                            y = result.groupValues[4].toInt(),
+                            z = result.groupValues[6].toInt(),
+                        ),
+                        to = Coord3d(
+                            x = result.groupValues[3].toInt(),
+                            y = result.groupValues[5].toInt(),
+                            z = result.groupValues[7].toInt(),
+                        ),
                     ),
                 )
             }
             ?: throw IllegalArgumentException("Could not parse reboot step: $line")
     }
 
-fun executeRebootSteps(rebootSteps: List<RebootStep>, min: Int, max: Int): Int =
-    rebootSteps
-        .filter {
-            min <= it.from.x && it.to.x <= max
-                    && min <= it.from.y && it.to.y <= max
-                    && min <= it.from.z && it.to.z <= max
-        }
+fun executeRebootSteps(rebootSteps: List<RebootStep>, min: Int, max: Int): Int {
+    val region = Cuboid(Coord3d(min, min, min), Coord3d(max, max, max))
+    return rebootSteps
+        .filter { region.intersects(it.cuboid) }
         .fold(mutableMapOf<Coord3d, Boolean>()) { map, rebootStep ->
-            rebootStep.getCubes().fold(map) { map2, coord3d -> 
-                map2[coord3d] = rebootStep.turnOn
-                map2
-            }
+            rebootStep.cuboid.getCubes()
+                .filter { region.contains(it) }
+                .fold(map) { map2, coord3d ->
+                    map2[coord3d] = rebootStep.turnOn
+                    map2
+                }
         }
         .count { it.value }
+}
