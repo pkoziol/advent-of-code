28d008222aa427a8d23143360670cebd3bda6589
koziolek <github@koziolek.biz>
2021-12-22 22:23:43 +0100

    Solution for 2021-12-22, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2021/day22/day22.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2021/day22/day22.kt
@@ -1,37 +1,109 @@
 package biz.koziolek.adventofcode.year2021.day22
 
 import biz.koziolek.adventofcode.*
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val inputFile = findInput(object {})
     val lines = inputFile.bufferedReader().readLines()
+
     val rebootSteps = parseReactorRebootSteps(lines)
+
     val onCubes = executeRebootSteps(rebootSteps, min = -50, max = 50)
-    println("On cubes: $onCubes")
+    println("On cubes in -50..50: $onCubes")
+
+    val onCubesAll = executeRebootStepsV2(rebootSteps, min = Int.MIN_VALUE, max = Int.MAX_VALUE)
+    println("On cubes everywhere: $onCubesAll")
 }
 
 data class RebootStep(val turnOn: Boolean, val cuboid: Cuboid)
 
-data class Cuboid(val from: Coord3d, val to: Coord3d) {
+sealed interface ICuboid {
+    fun exclude(cuboid: ICuboid): ICuboid
+    fun getCubes(): Sequence<Coord3d>
+    fun intersects(other: ICuboid): Boolean
+    fun intersection(other: ICuboid): ICuboid
+    fun countContained(): Long
+    operator fun contains(coord3d: Coord3d): Boolean
+}
 
-    fun getCubes(): Sequence<Coord3d> =
+object EmptyCuboid : ICuboid {
+    override fun exclude(cuboid: ICuboid) = EmptyCuboid
+    override fun getCubes() = emptySequence<Coord3d>()
+    override fun intersects(other: ICuboid) = false
+    override fun intersection(other: ICuboid) = EmptyCuboid
+    override fun countContained() = 0L
+    override fun contains(coord3d: Coord3d) = false
+}
+
+data class Cuboid(val from: Coord3d, val to: Coord3d, private val excluded: List<ICuboid> = emptyList()) : ICuboid {
+
+    override fun exclude(cuboid: ICuboid): Cuboid {
+        return when (val excludedIntersection = cuboid.intersection(this)) {
+            is Cuboid -> copy(excluded = excluded.map { it.exclude(excludedIntersection) } + excludedIntersection)
+            is EmptyCuboid -> this
+        }
+    }
+
+    override fun getCubes(): Sequence<Coord3d> =
         (from.x..to.x).asSequence().flatMap { x ->
             (from.y..to.y).asSequence().flatMap { y ->
                 (from.z..to.z).asSequence().map { z ->
                     Coord3d(x, y, z)
                 }
             }
+        }.filter { coord -> excluded.none { it.contains(coord) } }
+
+    override fun intersects(other: ICuboid) =
+        when (other) {
+            is Cuboid ->
+                this.from.x <= other.to.x && other.from.x <= this.to.x
+                        && this.from.y <= other.to.y && other.from.y <= this.to.y
+                        && this.from.z <= other.to.z && other.from.z <= this.to.z
+            is EmptyCuboid -> false
         }
 
-    fun intersects(other: Cuboid) =
-        this.from.x <= other.to.x && other.from.x <= this.to.x
-                && this.from.y <= other.to.y && other.from.y <= this.to.y
-                && this.from.z <= other.to.z && other.from.z <= this.to.z
+    override fun intersection(other: ICuboid): ICuboid =
+        if (intersects(other) && other is Cuboid) {
+            Cuboid(
+                from = Coord3d(
+                    x = max(this.from.x, other.from.x),
+                    y = max(this.from.y, other.from.y),
+                    z = max(this.from.z, other.from.z),
+                ),
+                to = Coord3d(
+                    x = min(this.to.x, other.to.x),
+                    y = min(this.to.y, other.to.y),
+                    z = min(this.to.z, other.to.z),
+                ),
+            )
+        } else {
+            EmptyCuboid
+        }
 
-    operator fun contains(coord3d: Coord3d) =
+    override fun countContained(): Long =
+        (to.x - from.x + 1L) * (to.y - from.y + 1L) * (to.z - from.z + 1L) - excluded.sumOf { it.countContained() }
+
+    override operator fun contains(coord3d: Coord3d) =
         from.x <= coord3d.x && coord3d.x <= to.x
                 && from.y <= coord3d.y && coord3d.y <= to.y
                 && from.z <= coord3d.z && coord3d.z <= to.z
+
+    override fun toString(): String {
+        return toString(indent = 0)
+    }
+
+    fun toString(indent: Int = 0): String {
+        val separator = "\n" + " ".repeat(indent + 2)
+        return "${from.x},${from.y},${from.z} -> ${to.x},${to.y},${to.z}" +
+                excluded.joinToString(separator = separator, prefix = separator) {
+                    when (it) {
+                        is Cuboid -> it.toString(indent + 2)
+                        else -> it.toString()
+                    }
+                }
+    }
 }
 
 fun parseReactorRebootSteps(lines: List<String>): List<RebootStep> =
@@ -71,4 +143,18 @@ fun executeRebootSteps(rebootSteps: List<RebootStep>, min: Int, max: Int): Int {
                 }
         }
         .count { it.value }
+}
+
+fun executeRebootStepsV2(rebootSteps: List<RebootStep>, min: Int, max: Int): Long {
+    val region = Cuboid(Coord3d(min, min, min), Coord3d(max, max, max))
+    return rebootSteps
+        .filter { region.intersects(it.cuboid) }
+        .fold(listOf<Cuboid>()) { list, rebootStep ->
+            if (rebootStep.turnOn) {
+                list.map { it.exclude(rebootStep.cuboid) } + rebootStep.cuboid
+            } else {
+                list.map { it.exclude(rebootStep.cuboid) }
+            }
+        }
+        .sumOf { it.countContained() }
 }
