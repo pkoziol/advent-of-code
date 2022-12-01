75801c2891865a651193743539e281fca74743a3
koziolek <github@koziolek.biz>
2022-12-01 09:07:49 +0100

    Solution for 2022-12-01, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2022/day01/day1.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2022/day01/day1.kt
@@ -4,8 +4,12 @@ import biz.koziolek.adventofcode.findInput
 
 fun main() {
     val inputFile = findInput(object {})
+
     val elves = parseElvesCalories(inputFile.bufferedReader().readLines())
     println("Elf with most calories has: ${findElfWithMostCalories(elves)?.calories?.sum()}")
+
+    val top3ElvesWithMostCalories = findTopElvesWithMostCalories(elves, count = 3)
+    println("Top 3 elves with most calories have: ${top3ElvesWithMostCalories.sumOf { it.calories.sum() }}")
 }
 
 data class Elf(val calories: List<Int>)
@@ -38,3 +42,8 @@ fun parseElvesCalories(lines: Iterable<String>): List<Elf> {
 
 fun findElfWithMostCalories(elves: List<Elf>): Elf? =
     elves.maxByOrNull { it.calories.sum() }
+
+fun findTopElvesWithMostCalories(elves: List<Elf>, count: Int): List<Elf> =
+    elves
+        .sortedByDescending { it.calories.sum() }
+        .take(count)
