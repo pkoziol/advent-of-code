56be55e4f9d9c85d454d33ba8a6606ac3521c16d
koziolek <github@koziolek.biz>
2022-12-06 11:13:08 +0100

    Add script that creates all files and downloads input

---
+++ scripts/templates/dayX.kt
@@ -0,0 +1,11 @@
+package biz.koziolek.adventofcode.year{YEAR}.day{DAY_LEADING_ZERO}
+
+import biz.koziolek.adventofcode.findInput
+
+fun main() {
+    val inputFile = findInput(object {})
+    val data = parse(inputFile.bufferedReader().readLines())
+}
+
+fun parse(lines: Iterable<String>): Unit =
+    TODO()
