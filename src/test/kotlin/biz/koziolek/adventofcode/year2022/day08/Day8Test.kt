b45c1a2a0bb8fbf79d570f7c3d02d865592aeb20
koziolek <github@koziolek.biz>
2022-12-08 16:15:39 +0100

    Solution for 2022-12-08, part 1

---
+++ src/test/kotlin/biz/koziolek/adventofcode/year2022/day08/Day8Test.kt
@@ -0,0 +1,50 @@
+package biz.koziolek.adventofcode.year2022.day08
+
+import biz.koziolek.adventofcode.Coord
+import biz.koziolek.adventofcode.findInput
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("2022")
+internal class Day8Test {
+
+    private val sampleInput = """
+            30373
+            25512
+            65332
+            33549
+            35390
+        """.trimIndent().split("\n")
+
+    @Test
+    fun testParseTrees() {
+        val trees = parseTrees(sampleInput)
+        assertEquals(7, trees[Coord(3, 0)])
+        assertEquals(9, trees[Coord(4, 3)])
+    }
+
+    @Test
+    fun testFindTreesIn4Directions() {
+        val trees = parseTrees(sampleInput)
+        val start = Coord(x = 0, y = 1)
+        assertEquals("3", getNorthTrees(trees, start).joinToString("") { it.value.toString() })
+        assertEquals("5512", getEastTrees(trees, start).joinToString("") { it.value.toString() })
+        assertEquals("633", getSouthTrees(trees, start).joinToString("") { it.value.toString() })
+        assertEquals("", getWestTrees(trees, start).joinToString("") { it.value.toString() })
+    }
+
+    @Test
+    fun testCountVisibleTrees() {
+        val trees = parseTrees(sampleInput)
+        assertEquals(21, countVisibleTreesFromEdges(trees))
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer1() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val trees = parseTrees(input)
+        assertEquals(1688, countVisibleTreesFromEdges(trees))
+    }
+}
