d1db4abad5ef424c1824028ffe101ac5e46abb73
koziolek <github@koziolek.biz>
2022-12-13 22:07:51 +0100

    Solution for 2022-12-13, part 1

---
+++ src/main/kotlin/biz/koziolek/adventofcode/year2022/day13/day13.kt
@@ -0,0 +1,111 @@
+package biz.koziolek.adventofcode.year2022.day13
+
+import biz.koziolek.adventofcode.findInput
+import java.util.ArrayDeque
+import java.util.Deque
+
+fun main() {
+    val inputFile = findInput(object {})
+    val packetPairs = parsePacketPairs(inputFile.bufferedReader().readLines())
+    println("Sum of packet pairs in right order: ${getSumOfPacketPairsInRightOrder(packetPairs)}")
+}
+
+sealed interface Packet : Comparable<Packet>
+
+data class IntPacket(val value: Int) : Packet {
+    override fun compareTo(other: Packet): Int =
+        when (other) {
+            is IntPacket -> this.value.compareTo(other.value)
+            is ListPacket -> ListPacket(this).compareTo(other)
+        }
+
+    override fun toString() = value.toString()
+}
+
+data class ListPacket(val children: List<Packet>) : Packet {
+    constructor(vararg ch: Packet) : this(ch.asList())
+
+    fun addChild(child: Packet): ListPacket =
+        copy(children = children + child)
+
+    override fun compareTo(other: Packet): Int =
+        when (other) {
+            is IntPacket -> this.compareTo(ListPacket(other))
+            is ListPacket -> {
+                val childComparison = this.children.zip(other.children)
+                    .fold(0) { acc, (first, second) ->
+                        if (acc != 0) {
+                            acc
+                        } else {
+                            first.compareTo(second)
+                        } 
+                    }
+
+                if (childComparison != 0) {
+                    childComparison
+                } else {
+                    this.children.size.compareTo(other.children.size)
+                }
+            }
+        }
+
+    override fun toString() = children.joinToString(",", "[", "]")
+}
+
+fun parsePacketPairs(lines: Iterable<String>): List<Pair<ListPacket, ListPacket>> =
+    lines.filter { it.isNotBlank() }
+        .chunked(2)
+        .map { (firstLine, secondLine) ->
+            parseListPacket(firstLine) to parseListPacket(secondLine)
+        }
+
+fun parseListPacket(line: String): ListPacket {
+    val stack: Deque<ListPacket> = ArrayDeque()
+    var isParsingNumber = false
+    var number = 0
+
+    fun pushChildUp() {
+        if (stack.size > 1) {
+            val inner = stack.pop()
+            val outer = stack.pop()
+            stack.push(outer.addChild(inner))
+        }
+    }
+
+    fun pushNumber() {
+        if (isParsingNumber) {
+            val list = stack.pop()
+            stack.push(list.addChild(IntPacket(number)))
+            isParsingNumber = false
+            number = 0
+        }
+    }
+
+    for (c in line) {
+        when (c) {
+            '[' -> stack.push(ListPacket())
+            ']' -> {
+                pushNumber()
+                pushChildUp()
+            }
+            ',' -> pushNumber()
+            in '0'..'9' -> {
+                isParsingNumber = true
+                number = number * 10 + c.digitToInt()
+            }
+            else -> throw IllegalStateException("Unexpected character: '$c'")
+        }
+    }
+
+    while (stack.size > 1) {
+        pushChildUp()
+    }
+
+    return stack.single()
+}
+
+fun getSumOfPacketPairsInRightOrder(packetPairs: List<Pair<Packet, Packet>>): Int =
+    packetPairs
+        .withIndex()
+        .filter { (_, pair) -> pair.first < pair.second }
+        .sumOf { it.index + 1 }
---
+++ src/test/kotlin/biz/koziolek/adventofcode/year2022/day13/Day13Test.kt
@@ -0,0 +1,128 @@
+package biz.koziolek.adventofcode.year2022.day13
+
+import biz.koziolek.adventofcode.findInput
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("2022")
+internal class Day13Test {
+
+    private val sampleInput = """
+            [1,1,3,1,1]
+            [1,1,5,1,1]
+
+            [[1],[2,3,4]]
+            [[1],4]
+
+            [9]
+            [[8,7,6]]
+
+            [[4,4],4,4]
+            [[4,4],4,4,4]
+
+            [7,7,7,7]
+            [7,7,7]
+
+            []
+            [3]
+
+            [[[]]]
+            [[]]
+
+            [1,[2,[3,[4,[5,6,7]]]],8,9]
+            [1,[2,[3,[4,[5,6,0]]]],8,9]
+        """.trimIndent().split("\n")
+
+    @Test
+    fun testParsePackets() {
+        val packetPairs = parsePacketPairs(sampleInput)
+        assertEquals(8, packetPairs.size)
+
+        assertEquals(
+            ListPacket(
+                IntPacket(1),
+                IntPacket(1),
+                IntPacket(3),
+                IntPacket(1),
+                IntPacket(1),
+            ),
+            packetPairs[0].first
+        )
+        assertEquals(
+            ListPacket(
+                IntPacket(1),
+                IntPacket(1),
+                IntPacket(5),
+                IntPacket(1),
+                IntPacket(1),
+            ),
+            packetPairs[0].second
+        )
+
+        assertEquals(
+            ListPacket(
+                ListPacket(
+                    IntPacket(1),
+                ),
+                ListPacket(
+                    IntPacket(2),
+                    IntPacket(3),
+                    IntPacket(4),
+                ),
+            ),
+            packetPairs[1].first
+        )
+        assertEquals(
+            ListPacket(
+                ListPacket(
+                    IntPacket(1),
+                ),
+                IntPacket(4),
+            ),
+            packetPairs[1].second
+        )
+
+        assertEquals(
+            ListPacket(
+                ListPacket(
+                    ListPacket(),
+                ),
+            ),
+            packetPairs[6].first
+        )
+        assertEquals(
+            ListPacket(
+                ListPacket(),
+            ),
+            packetPairs[6].second
+        )
+    }
+
+    @Test
+    fun testPacketComparison() {
+        val packetPairs = parsePacketPairs(sampleInput)
+        assertTrue(packetPairs[0].first < packetPairs[0].second)
+        assertTrue(packetPairs[1].first < packetPairs[1].second)
+        assertTrue(packetPairs[2].first > packetPairs[2].second)
+        assertTrue(packetPairs[3].first < packetPairs[3].second)
+        assertTrue(packetPairs[4].first > packetPairs[4].second)
+        assertTrue(packetPairs[5].first < packetPairs[5].second)
+        assertTrue(packetPairs[6].first > packetPairs[6].second)
+        assertTrue(packetPairs[7].first > packetPairs[7].second)
+    }
+
+    @Test
+    fun testGetSumOfPacketPairsInRightOrder() {
+        val packetPairs = parsePacketPairs(sampleInput)
+        assertEquals(13, getSumOfPacketPairsInRightOrder(packetPairs))
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer1() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val packetPairs = parsePacketPairs(input)
+        assertEquals(6369, getSumOfPacketPairsInRightOrder(packetPairs))
+    }
+}
