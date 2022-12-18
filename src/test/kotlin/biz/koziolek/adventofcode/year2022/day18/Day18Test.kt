c5dcb03f9583893f470ebe18449dd0daec3e0abd
koziolek <github@koziolek.biz>
2022-12-18 17:27:41 +0100

    Solution for 2022-12-18, part 1

---
+++ src/test/kotlin/biz/koziolek/adventofcode/year2022/day18/Day18Test.kt
@@ -0,0 +1,46 @@
+package biz.koziolek.adventofcode.year2022.day18
+
+import biz.koziolek.adventofcode.findInput
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("2022")
+internal class Day18Test {
+
+    private val sampleInput = """
+            2,2,2
+            1,2,2
+            3,2,2
+            2,1,2
+            2,3,2
+            2,2,1
+            2,2,3
+            2,2,4
+            2,2,6
+            1,2,5
+            3,2,5
+            2,1,5
+            2,3,5
+        """.trimIndent().split("\n")
+
+    @Test
+    fun testParseDroplet() {
+        val droplet = parseDroplet(sampleInput)
+        assertEquals(13, droplet.size)
+    }
+
+    @Test
+    fun testGetSurfaceArea() {
+        val droplet = parseDroplet(sampleInput)
+        assertEquals(64, getSurfaceArea(droplet))
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer1() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val droplet = parseDroplet(input)
+        assertEquals(4628, getSurfaceArea(droplet))
+    }
+}
