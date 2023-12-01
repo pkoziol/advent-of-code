ea7b7ec188cff9cad56fa7883d5f83bbfb7ec5e1
koziolek <github@koziolek.biz>
2023-12-01 10:35:37 +0100

    Solution for 2023-12-01, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day01/day1.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day01/day1.kt
@@ -4,8 +4,9 @@ import biz.koziolek.adventofcode.findInput
 
 fun main() {
     val inputFile = findInput(object {})
-    val data = parseCalibrationValues(inputFile.bufferedReader().readLines())
-    println("Sum of all calibration values is: ${sumCalibrationValues(data)}")
+    val lines = inputFile.bufferedReader().readLines()
+    println("Sum of all calibration values is: ${sumCalibrationValues(parseCalibrationValues(lines))}")
+    println("Sum of all calibration values including spelled numbers is: ${sumCalibrationValues(parseCalibrationValuesWithLetters(lines))}")
 }
 
 fun parseCalibrationValues(lines: Iterable<String>): List<Int> =
@@ -14,3 +15,29 @@ fun parseCalibrationValues(lines: Iterable<String>): List<Int> =
         .map { it.first().digitToInt() * 10 + it.last().digitToInt() }
 
 fun sumCalibrationValues(values: List<Int>) = values.sum()
+
+private val spelledDigits = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
+
+private val stringsToFind = spelledDigits.keys + (1..9).map { it.toString() }
+
+fun parseCalibrationValuesWithLetters(lines: Iterable<String>): List<Int> =
+    lines
+        .asSequence()
+        .map { line -> line.findAnyOf(stringsToFind) to line.findLastAnyOf(stringsToFind) }
+        .map { toDigit(it.first?.second) to toDigit(it.second?.second) }
+        .filter { it.first != null && it.second != null }
+        .map { it.first!! * 10 + it.second!! }
+        .toList()
+
+private fun toDigit(str: String?): Int? =
+    spelledDigits[str] ?: str?.toInt()
