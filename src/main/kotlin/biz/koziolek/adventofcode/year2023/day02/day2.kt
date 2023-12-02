78984636b34e0ad9758f77202a86dd4765d75ec3
koziolek <github@koziolek.biz>
2023-12-02 12:01:20 +0100

    Solution for 2023-12-02, part 1

---
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day02/day2.kt
@@ -0,0 +1,52 @@
+package biz.koziolek.adventofcode.year2023.day02
+
+import biz.koziolek.adventofcode.findInput
+
+fun main() {
+    val inputFile = findInput(object {})
+    val games = parseGames(inputFile.bufferedReader().readLines())
+    val possible = findPossibleGames(games, FULL_SET)
+    println("Possible games are: ${possible.map { it.id }} with sum of IDs: ${sumGameIds(possible)}")
+}
+
+val FULL_SET = CubeSet(red = 12, green = 13, blue = 14)
+
+data class Game(val id: Int, val sets: List<CubeSet>)
+data class CubeSet(val red: Int = 0, val green: Int = 0, val blue: Int = 0) {
+    fun add(count: Int, color: String): CubeSet {
+        val (newRed, newGreen, newBlue) = when (color) {
+            "red" -> Triple(count, 0, 0)
+            "green" -> Triple(0, count, 0)
+            "blue" -> Triple(0, 0, count)
+            else -> throw IllegalArgumentException("Unknown color: $color")
+        }
+        return copy(
+            red = red + newRed,
+            green = green + newGreen,
+            blue = blue + newBlue,
+        )
+    }
+
+    fun isSubSetOf(other: CubeSet) =
+        this.red <= other.red
+                && this.green <= other.green
+                && this.blue <= other.blue
+}
+
+fun parseGames(lines: Iterable<String>): List<Game> =
+    lines.map { line ->
+        val parts = line.split(Regex("[:;]"))
+        val id = parts[0].replace("Game ", "").toInt()
+        val sets = parts.drop(1).map { setStr ->
+            setStr.split(",").fold(CubeSet()) { acc, colorStr ->
+                val (count, color) = colorStr.trim().split(" ")
+                acc.add(count.toInt(), color)
+            }
+        }
+        Game(id, sets)
+    }
+
+fun findPossibleGames(games: List<Game>, fullSet: CubeSet): List<Game> =
+    games.filter { game -> game.sets.all { set -> set.isSubSetOf(fullSet) } }
+
+fun sumGameIds(games: List<Game>) = games.sumOf { it.id }
