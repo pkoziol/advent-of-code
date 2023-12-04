539b42bffcd3e0abdd41ef78c2d16649b996ef45
koziolek <github@koziolek.biz>
2023-12-04 10:04:34 +0100

    Solution for 2023-12-04, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day04/day4.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day04/day4.kt
@@ -7,9 +7,16 @@ fun main() {
     val inputFile = findInput(object {})
     val scratchCards = parseScratchCards(inputFile.bufferedReader().readLines())
     println("Total points: ${scratchCards.sumOf { it.points }}")
+    val finalScratchCards = winMoreScratchCards(scratchCards)
+    println("Total cards after winning more: ${finalScratchCards.sumOf { it.count }}")
 }
 
-data class ScratchCard(val id: Int, val winningNumbers: List<Int>, val myNumbers: List<Int>) {
+data class ScratchCard(
+    val id: Int,
+    val winningNumbers: List<Int>,
+    val myNumbers: List<Int>,
+    val count: Int = 1
+) {
     val myWinningNumbers: Set<Int> = myNumbers.intersect(winningNumbers)
 
     val points: Int
@@ -39,3 +46,19 @@ fun parseScratchCards(lines: Iterable<String>): List<ScratchCard> =
                 .map { it.trim().toInt() },
         )
     }
+
+fun winMoreScratchCards(scratchCards: List<ScratchCard>): List<ScratchCard> =
+    scratchCards
+        .associateBy { it.id }
+        .let { map ->
+            map.keys.fold(map) { newMap, id ->
+                val currentCard = newMap[id]!!
+                val wonCardIds = (currentCard.id + 1)..(currentCard.id + currentCard.myWinningNumbers.size)
+                val updates = wonCardIds
+                    .mapNotNull { newMap[it] }
+                    .map { it.copy(count = it.count + currentCard.count) }
+                    .associateBy { it.id }
+                newMap + updates
+            }
+        }
+        .values.toList()
