ec5695bdc4fb97be4b908d927ad56a99aba18cb7
koziolek <github@koziolek.biz>
2023-12-05 11:57:04 +0100

    Suuuper slow solution for 2023-12-05, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day05/day5.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day05/day5.kt
@@ -4,12 +4,18 @@ import biz.koziolek.adventofcode.findInput
 
 fun main() {
     val inputFile = findInput(object {})
-    val almanac = parseAlmanac(inputFile.bufferedReader().readLines())
+    val almanac = parseAlmanacV1(inputFile.bufferedReader().readLines())
     println("Lowest location is: ${almanac.mapAll(from = "seed", to = "location", items = almanac.seeds).min()}")
 }
 
-fun parseAlmanac(lines: Iterable<String>): Almanac {
-    var seeds: List<Long> = emptyList()
+fun parseAlmanacV1(lines: Iterable<String>): Almanac =
+    parseAlmanac(lines, ::parseSeedsV1)
+
+fun parseAlmanacV2(lines: Iterable<String>): Almanac =
+    parseAlmanac(lines, ::parseSeedsV2)
+
+private fun parseAlmanac(lines: Iterable<String>, seedParser: (String) -> Sequence<Long>): Almanac {
+    var seeds: Sequence<Long> = emptySequence()
     val maps: MutableList<AlmanacMap> = mutableListOf()
 
     val iterator = lines.iterator()
@@ -17,11 +23,7 @@ fun parseAlmanac(lines: Iterable<String>): Almanac {
         val line = iterator.next()
 
         if (line.startsWith("seeds:")) {
-            seeds = line
-                .replace("seeds:", "")
-                .trim()
-                .split(" ")
-                .map { it.trim().toLong() }
+            seeds = seedParser(line)
         } else if (line.contains("map:")) {
             val (from, _, to, _) = line.split("-", " ")
             val map = buildList {
@@ -53,12 +55,32 @@ fun parseAlmanac(lines: Iterable<String>): Almanac {
     return Almanac(seeds, maps)
 }
 
-data class Almanac(val seeds: List<Long>, val maps: List<AlmanacMap>) {
+private fun parseSeedsV1(line: String) =
+    line
+        .replace("seeds:", "")
+        .trim()
+        .split(" ")
+        .map { it.trim().toLong() }
+        .asSequence()
+
+private fun parseSeedsV2(line: String) =
+    line
+        .replace("seeds:", "")
+        .trim()
+        .split(" ")
+        .map { it.trim().toLong() }
+        .chunked(2)
+        .asSequence()
+        .flatMap { (start, length) ->
+            start until start + length
+        }
+
+data class Almanac(val seeds: Sequence<Long>, val maps: List<AlmanacMap>) {
     fun findMap(from: String, to: String? = null): AlmanacMap? =
         maps.singleOrNull { it.sourceCategory == from
                 && (to == null || it.destinationCategory == to) }
 
-    fun mapAll(from: String, to: String, items: List<Long>): List<Long> {
+    fun mapAll(from: String, to: String, items: Sequence<Long>): Sequence<Long> {
         val mapsToUse = buildList {
             var next: String? = from
 
