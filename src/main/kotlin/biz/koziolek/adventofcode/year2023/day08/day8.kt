bf393910be02479378612e39ed1b3b08df688a85
koziolek <github@koziolek.biz>
2023-12-08 10:07:50 +0100

    Solution for 2023-12-08, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day08/day8.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day08/day8.kt
@@ -6,69 +6,140 @@ fun main() {
     val inputFile = findInput(object {})
     val map = parseWastelandMap(inputFile.bufferedReader().readLines())
     println("It takes ${map.followInstructions().size - 1} moves to move from $START_ID to $END_ID")
+    println("It takes ${map.followGhostInstructions()} moves to move from $START_ID to $END_ID")
 }
 
 const val START_ID = "AAA"
 const val END_ID = "ZZZ"
-const val LEFT_WEIGHT = -1
-const val RIGHT_WEIGHT = 1
 
 data class WastelandMap(
     val instructions: String,
-    val graph: Graph<WastelandNode, UniDirectionalGraphEdge<WastelandNode>>
+    val nodes: Map<String, WastelandNode>
 ) {
-    @Suppress("MoveVariableDeclarationIntoWhen")
-    fun followInstructions(): List<WastelandNode> = buildList {
+    val ghostStartNodes = nodes.values.filter { it.isGhostStart }.sortedBy { it.id }
+    val ghostEndNodes = nodes.values.filter { it.isGhostEnd }.sortedBy { it.id }
+
+    fun followInstructions(): List<WastelandNode> =
+        findPath(START_ID) { node -> node.id == END_ID }
+
+    private fun findPath(startId: String, isEnd: (WastelandNode) -> Boolean): List<WastelandNode> = buildList {
         var movesCount = 0
-        var currentNode = graph.nodes.single { it.id == START_ID }
+        var currentNode = nodes.values.single { it.id == startId }
         add(currentNode)
 
-        while (currentNode.id != END_ID) {
+        while (!isEnd(currentNode)) {
             val move = instructions[movesCount % instructions.length]
-            val edge = when (move) {
-                'L' -> graph.edges.single { it.node1 == currentNode && it.weight == LEFT_WEIGHT }
-                'R' -> graph.edges.single { it.node1 == currentNode && it.weight == RIGHT_WEIGHT }
-                else -> throw IllegalArgumentException("Unknown move: $move")
-            }
-
-            currentNode = edge.node2
+            currentNode = findNext(currentNode, move)
             add(currentNode)
 
             movesCount++
         }
     }
+
+    @Deprecated("Should work I think, but toList() will eat ALL your memory. will take FOREVER and there's sequence overhead.")
+    fun followGhostInstructions1(): Sequence<List<WastelandNode>> = sequence {
+        var movesCount = 0
+        var currentNodes = ghostStartNodes
+        yield(currentNodes)
+
+        while (!currentNodes.all { it.isGhostEnd }) {
+            val move = instructions[movesCount % instructions.length]
+            currentNodes = currentNodes.map { findNext(it, move) }
+            yield(currentNodes)
+
+            movesCount++
+        }
+    }
+
+    @Deprecated("Should work I think, with no memory issues, but takes FOREVER.")
+    fun followGhostInstructions2(): Long {
+        var movesCount = 0L
+        var currentNodes = ghostStartNodes
+        val found = mutableSetOf<Int>()
+
+        println("Instructions len: ${instructions.length}")
+
+        while (!currentNodes.all { it.isGhostEnd }) {
+            val move = instructions[(movesCount % instructions.length).toInt()]
+            currentNodes = currentNodes.map { findNext(it, move) }
+
+            movesCount++
+
+            currentNodes.forEachIndexed { index, node ->
+                if (node.isGhostEnd) {
+                    println("$index found ${node.id} in $movesCount % ${instructions.length} = ${movesCount % instructions.length}")
+                    found.add(index)
+                }
+            }
+
+            if (found.size == 6) {
+                println("Found all!")
+                found.add(11111)
+            }
+
+            if (movesCount % 10000000 == 0L) {
+                println(movesCount)
+            }
+            if (movesCount > 100000) {
+                break
+            }
+        }
+
+        return movesCount
+    }
+
+    fun followGhostInstructions(): Long {
+        val counts = ghostStartNodes
+            .map { node ->
+                findPath(node.id) { possibleEndNode ->
+                    possibleEndNode.isGhostEnd
+                }.size - 1
+            }
+            .map { it.toLong() }
+
+        if (counts.any { it % instructions.length != 0L }) {
+            throw AssertionError("Counts are not multiply of instructions length - I don't know what to do")
+        }
+
+        return lcm(counts)
+    }
+
+    private fun findNext(node: WastelandNode, move: Char) =
+        when (move) {
+            'L' -> node.left!!
+            'R' -> node.right!!
+            else -> throw IllegalArgumentException("Unknown move: $move")
+        }
 }
 
-data class WastelandNode(override val id: String) : GraphNode {
-    override fun toGraphvizString(): String = id
+data class WastelandNode(val id: String) {
+    var left: WastelandNode? = null
+    var right: WastelandNode? = null
+    val isGhostStart = id[2] == 'A'
+    val isGhostEnd = id[2] == 'Z'
 }
 
 fun parseWastelandMap(lines: List<String>): WastelandMap {
     val instructions = lines.first
-    val graph = buildGraph<WastelandNode, UniDirectionalGraphEdge<WastelandNode>> {
-        val regex = Regex("(?<start>[A-Z]+) = \\((?<left>[A-Z]+), (?<right>[A-Z]+)\\)")
+    val regex = Regex("(?<start>[A-Z0-9]+) = \\((?<left>[A-Z0-9]+), (?<right>[A-Z0-9]+)\\)")
 
-        val nodes = lines
-            .drop(2)
-            .mapNotNull { regex.find(it) }
-            .map { WastelandNode(it.groups["start"]!!.value) }
-            .associateBy { it.id }
+    val nodes = lines
+        .drop(2)
+        .mapNotNull { regex.find(it) }
+        .map { WastelandNode(it.groups["start"]!!.value) }
+        .associateBy { it.id }
 
-        lines
-            .drop(2)
-            .mapNotNull { regex.find(it) }
-            .flatMap { match ->
-                val startNode = nodes[match.groups["start"]!!.value]!!
-                val leftNode = nodes[match.groups["left"]!!.value]!!
-                val rightNode = nodes[match.groups["right"]!!.value]!!
+    lines
+        .drop(2)
+        .mapNotNull { regex.find(it) }
+        .forEach { match ->
+            val startNode = nodes[match.groups["start"]!!.value]!!
+            val leftNode = nodes[match.groups["left"]!!.value]!!
+            val rightNode = nodes[match.groups["right"]!!.value]!!
 
-                listOf(
-                    UniDirectionalGraphEdge(startNode, leftNode, LEFT_WEIGHT),
-                    UniDirectionalGraphEdge(startNode, rightNode, RIGHT_WEIGHT),
-                )
-            }
-            .forEach { add(it) }
-    }
+            startNode.left = leftNode
+            startNode.right = rightNode
+        }
 
-    return WastelandMap(instructions, graph)
+    return WastelandMap(instructions, nodes)
 }
--- src/test/kotlin/biz/koziolek/adventofcode/year2023/day08/Day8Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2023/day08/Day8Test.kt
@@ -28,17 +28,28 @@ internal class Day8Test {
             ZZZ = (ZZZ, ZZZ)
         """.trimIndent().split("\n")
 
+    private val sampleInput3 = """
+            LR
+
+            11A = (11B, XXX)
+            11B = (XXX, 11Z)
+            11Z = (11B, XXX)
+            22A = (22B, XXX)
+            22B = (22C, 22C)
+            22C = (22Z, 22Z)
+            22Z = (22B, 22B)
+            XXX = (XXX, XXX)
+        """.trimIndent().split("\n")
+
     @Test
     fun testParseWastelandMap() {
         val map1 = parseWastelandMap(sampleInput1)
         assertEquals("RL", map1.instructions)
-        assertEquals(7, map1.graph.nodes.size)
-        assertEquals(14, map1.graph.edges.size)
+        assertEquals(7, map1.nodes.size)
 
         val map2 = parseWastelandMap(sampleInput2)
         assertEquals("LLR", map2.instructions)
-        assertEquals(3, map2.graph.nodes.size)
-        assertEquals(6, map2.graph.edges.size)
+        assertEquals(3, map2.nodes.size)
     }
 
     @Test
@@ -62,5 +73,47 @@ internal class Day8Test {
         val input = findInput(object {}).bufferedReader().readLines()
         val map = parseWastelandMap(input)
         assertEquals(15871, map.followInstructions().size - 1)
+    }
+
+    @Test
+    fun testGhostNodes() {
+        val map = parseWastelandMap(sampleInput3)
+        assertEquals(8, map.nodes.size)
+        assertEquals(
+            listOf("11A", "22A"),
+            map.ghostStartNodes.map { it.id }
+        )
+        assertEquals(
+            listOf("11Z", "22Z"),
+            map.ghostEndNodes.map { it.id }
+        )
+    }
+
+    @Test
+    fun testSampleAnswer2() {
+        val map = parseWastelandMap(sampleInput3)
+        val expectedPaths = listOf(
+            listOf("11A", "22A"),
+            listOf("11B", "22B"),
+            listOf("11Z", "22C"),
+            listOf("11B", "22Z"),
+            listOf("11Z", "22B"),
+            listOf("11B", "22C"),
+            listOf("11Z", "22Z"),
+        )
+        assertEquals(
+            expectedPaths,
+            map.followGhostInstructions1().map { it.map { node -> node.id } }.toList()
+        )
+        assertEquals(6, map.followGhostInstructions2())
+    }
+
+    @Test
+    @Tag("answer")
+//    @Disabled
+    fun testAnswer2() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val map = parseWastelandMap(input)
+        assertEquals(11283670395017, map.followGhostInstructions())
     }
 }
