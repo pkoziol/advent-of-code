906bfbc2fb4a80a5b190e8c165ff20579ae9557f
koziolek <github@koziolek.biz>
2023-12-10 09:44:27 +0100

    Solution for 2023-12-10, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day10/day10.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day10/day10.kt
@@ -1,12 +1,11 @@
 package biz.koziolek.adventofcode.year2023.day10
 
-import biz.koziolek.adventofcode.Coord
-import biz.koziolek.adventofcode.findInput
-import biz.koziolek.adventofcode.getAdjacentCoords
+import biz.koziolek.adventofcode.*
 
 fun main() {
     val inputFile = findInput(object {})
     val maze = parsePipeMaze(inputFile.bufferedReader().readLines())
+    println("Farthest distance from the start on the loop: ${maze.theLoopFarthestDistanceFromStart}")
     println("Farthest distance from the start on the loop: ${maze.theLoopFarthestDistanceFromStart}")
 }
 
@@ -41,6 +40,15 @@ data class PipeMaze(val contents: Map<Coord, Char>) {
 
     val theLoopFarthestDistanceFromStart: Int by lazy { theLoop.size / 2 }
 
+    val insideTheLoop: Set<Coord> by lazy {
+        val filter = contents.keys
+            .filter { !theLoop.keys.contains(it) }
+        filter
+            .filter { findEdgeIntersections(it) % 2 == 1 }
+            .map { it }
+            .toSet()
+    }
+
     fun findConnectedTo(coord: Coord): Set<Coord> =
         when (contents[coord]) {
             NORTH_SOUTH -> setOf(coord + Coord(0, -1), coord + Coord(0, 1))
@@ -55,6 +63,75 @@ data class PipeMaze(val contents: Map<Coord, Char>) {
                 .toSet()
             else -> emptySet()
         }
+
+    fun findEdgeIntersections(coord: Coord): Int {
+        val map = (0 until coord.y)
+            .map { y -> Coord(coord.x, y) }
+        val loopWithoutStart = replaceStart(theLoop)
+        val ray = map
+            .mapNotNull { loopWithoutStart[it] }
+        val rayStr = ray.joinToString("")
+
+//        val countEW = ray.count { it == EAST_WEST }
+//        val countNE = ray.count { it == NORTH_EAST }
+//        val countNW = ray.count { it == NORTH_WEST }
+//        val countSW = ray.count { it == SOUTH_WEST }
+//        val countSE = ray.count { it == SOUTH_EAST }
+//        val a = countSE + countNW
+//        val aa = if (a > 0) { a - 1 } else { 0 }
+//        val b = countSW + countNE
+//        val bb = if (b > 0) { b - 1 } else { 0 }
+//        val intersections = countEW + aa + bb
+
+        val singleCrossing = Regex("-|F\\|*J|7\\|*L")
+            .findAll(rayStr)
+            .map { it.value }
+            .toList()
+        val doubleCrossing = Regex("F\\|*L|7\\|*J")
+            .findAll(rayStr)
+            .map { it.value }
+            .toList()
+        val intersections = 0 + singleCrossing.size + doubleCrossing.size * 2
+
+        return intersections
+    }
+
+    fun replaceStart(pipes: Map<Coord, Char>): Map<Coord, Char> {
+        val start = pipes.entries.singleOrNull { it.value == START }
+        return if (start != null) {
+            val startCoord = start.key
+            val connectedTo = findConnectedTo(startCoord)
+            if (connectedTo.size != 2) {
+                throw IllegalArgumentException("Not 2?!")
+            }
+
+            val leftOrRight = connectedTo.filter { it.y == startCoord.y }
+            val upOrDown = connectedTo.filter { it.x == startCoord.x }
+            val replacement = if (upOrDown.size == 2) {
+                NORTH_SOUTH
+            } else if (leftOrRight.size == 2) {
+                EAST_WEST
+            } else {
+                val diff = Coord(
+                    x = leftOrRight.single().x - startCoord.x,
+                    y = upOrDown.single().y - startCoord.y,
+                )
+
+                when (diff) {
+                    Coord(1, -1) -> NORTH_EAST
+                    Coord(-1, -1) -> NORTH_WEST
+                    Coord(-1, 1) -> SOUTH_WEST
+                    Coord(1, 1) -> SOUTH_EAST
+                    else -> throw IllegalArgumentException("Unexpected diff: $diff")
+                }
+            }
+
+            pipes + (startCoord to replacement)
+        } else {
+            pipes
+        }
+    }
+
 }
 
 fun parsePipeMaze(lines: Iterable<String>): PipeMaze =
@@ -66,3 +143,34 @@ fun parsePipeMaze(lines: Iterable<String>): PipeMaze =
         }
         .toMap()
         .let { PipeMaze(it) }
+
+fun mazeToString(maze: PipeMaze,
+                 replaceStart: Boolean = false,
+                 defaultColor: AsciiColor = AsciiColor.BLACK,
+                 loopColor: AsciiColor? = null,
+                 insideColor: AsciiColor? = null) =
+    pipesToString(
+        if (replaceStart) maze.replaceStart(maze.contents) else maze.contents,
+        highlights = maze.contents.keys.associateWith { defaultColor }
+                + (if (loopColor != null ) maze.theLoop.keys.associateWith { loopColor } else emptyMap())
+                + (if (insideColor != null ) maze.insideTheLoop.associateWith { insideColor } else emptyMap())
+    )
+
+fun pipesToString(pipes: Map<Coord, Char>,
+                  highlights: Map<Coord, AsciiColor> = emptyMap()) =
+    buildString {
+        val width = pipes.getWidth()
+        val height = pipes.getHeight()
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val coord = Coord(x, y)
+                val symbol = pipes[coord] ?: ' '
+                append(highlights[coord]?.format(symbol) ?: symbol)
+            }
+
+            if (y != height - 1) {
+                append("\n")
+            }
+        }
+    }
--- src/test/kotlin/biz/koziolek/adventofcode/year2023/day10/Day10Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2023/day10/Day10Test.kt
@@ -1,5 +1,6 @@
 package biz.koziolek.adventofcode.year2023.day10
 
+import biz.koziolek.adventofcode.AsciiColor.*
 import biz.koziolek.adventofcode.Coord
 import biz.koziolek.adventofcode.findInput
 import org.junit.jupiter.api.Assertions.*
@@ -39,6 +40,44 @@ internal class Day10Test {
             SJLL7
             |F--J
             LJ.LJ
+        """.trimIndent().split("\n")
+
+    private val sampleInput3 = """
+            ..........
+            .S------7.
+            .|F----7|.
+            .||....||.
+            .||....||.
+            .|L-7F-J|.
+            .|..||..|.
+            .L--JL--J.
+            ..........
+        """.trimIndent().split("\n")
+
+    private val sampleInput4 = """
+            .F----7F7F7F7F-7....
+            .|F--7||||||||FJ....
+            .||.FJ||||||||L7....
+            FJL7L7LJLJ||LJ.L-7..
+            L--J.L7...LJS7F-7L7.
+            ....F-J..F7FJ|L7L7L7
+            ....L7.F7||L7|.L7L7|
+            .....|FJLJ|FJ|F7|.LJ
+            ....FJL-7.||.||||...
+            ....L---J.LJ.LJLJ...
+        """.trimIndent().split("\n")
+
+    private val sampleInput5 = """
+            FF7FSF7F7F7F7F7F---7
+            L|LJ||||||||||||F--J
+            FL-7LJLJ||||||LJL-77
+            F--JF--7||LJLJ7F7FJ-
+            L---JF-JLJ.||-FJLJJ7
+            |F|F-JF---7F7-L7L|7|
+            |FFJF7L7F-JF7|JL---7
+            7-L-JL7||F7|L7F-7F7|
+            L.L7LFJ|||||FJL7||LJ
+            L7JLJL-JLJLJL--JLJ.L
         """.trimIndent().split("\n")
 
     @Test
@@ -96,5 +135,37 @@ internal class Day10Test {
         val input = findInput(object {}).bufferedReader().readLines()
         val maze = parsePipeMaze(input)
         assertEquals(6846, maze.theLoopFarthestDistanceFromStart)
+    }
+
+    @Test
+    fun testSampleAnswer2() {
+        val maze3 = parsePipeMaze(sampleInput3)
+        debugPrintMaze(maze3)
+        assertEquals(4, maze3.insideTheLoop.size)
+
+        val maze4 = parsePipeMaze(sampleInput4)
+        debugPrintMaze(maze4)
+        assertEquals(8, maze4.insideTheLoop.size)
+
+        val maze5 = parsePipeMaze(sampleInput5)
+        debugPrintMaze(maze5)
+        assertEquals(10, maze5.insideTheLoop.size)
+    }
+
+    private fun debugPrintMaze(maze: PipeMaze) {
+        println(mazeToString(maze, loopColor = WHITE))
+        println()
+        println(mazeToString(maze, loopColor = WHITE, replaceStart = true))
+        println()
+        println(mazeToString(maze, loopColor = WHITE, replaceStart = true, insideColor = BRIGHT_WHITE))
+        println()
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer2() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val maze = parsePipeMaze(input)
+        assertEquals(325, maze.insideTheLoop.size)
     }
 }
