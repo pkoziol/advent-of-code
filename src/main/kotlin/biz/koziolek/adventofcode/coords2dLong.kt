e91d73719e4aacb250fc76a447a84d8643882b8f
koziolek <github@koziolek.biz>
2023-12-18 23:26:08 +0100

    Solution for 2023-12-18, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/coords2dLong.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/coords2dLong.kt
@@ -19,7 +19,18 @@ data class LongCoord(val x: Long, val y: Long) {
     infix fun manhattanDistanceTo(other: LongCoord): Long =
         abs(x - other.x) + abs(y - other.y)
 
+    fun move(direction: Direction, count: Long = 1): LongCoord =
+        when (direction) {
+            Direction.NORTH -> copy(y = y - count)
+            Direction.SOUTH -> copy(y = y + count)
+            Direction.WEST -> copy(x = x - count)
+            Direction.EAST -> copy(x = x + count)
+        }
+
     companion object {
+        val yComparator: Comparator<LongCoord> = Comparator.comparing { it.y }
+        val xComparator: Comparator<LongCoord> = Comparator.comparing { it.x }
+
         fun fromString(str: String): LongCoord =
             str.split(',')
                 .map { it.toInt() }
@@ -32,6 +43,30 @@ fun LongProgression.zipAsCoord(ys: LongProgression) = zip(ys) { x, y -> LongCoor
 fun <T> Map<LongCoord, T>.getWidth() = keys.maxOfOrNull { it.x }?.plus(1) ?: 0
 
 fun <T> Map<LongCoord, T>.getHeight() = keys.maxOfOrNull { it.y }?.plus(1) ?: 0
+
+fun Iterable<LongCoord>.sortByYX() =
+    sortedWith(
+        LongCoord.yComparator.thenComparing(LongCoord.xComparator)
+    )
+
+fun <T> Map<LongCoord, T>.to2DString(formatter: (LongCoord, T?) -> Char): String =
+    to2DStringOfStrings { coord, t -> formatter(coord, t).toString() }
+
+fun <T> Map<LongCoord, T>.to2DStringOfStrings(formatter: (LongCoord, T?) -> String): String =
+    buildString {
+        val (minX, maxX) = keys.minAndMaxOrNull { it.x }!!
+        val (minY, maxY) = keys.minAndMaxOrNull { it.y }!!
+        for (y in minY..maxY) {
+            for (x in minX..maxX) {
+                val coord = LongCoord(x, y)
+                val value = get(coord)
+                append(formatter(coord, value))
+            }
+            if (y != maxY) {
+                append('\n')
+            }
+        }
+    }
 
 fun <T> Map<LongCoord, T>.getAdjacentCoords(coord: LongCoord, includeDiagonal: Boolean): Set<LongCoord> =
     keys.getAdjacentCoords(coord, includeDiagonal)
--- src/test/kotlin/biz/koziolek/adventofcode/year2023/day18/Day18Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2023/day18/Day18Test.kt
@@ -30,20 +30,20 @@ internal class Day18Test {
         val digPlan = parseDigPlan(sampleInput)
         assertEquals(
             listOf(
-                DigPlanEntry(direction = Direction.EAST, distance = 6, color = "#70c710"),
-                DigPlanEntry(direction = Direction.SOUTH, distance = 5, color = "#0dc571"),
-                DigPlanEntry(direction = Direction.WEST, distance = 2, color = "#5713f0"),
-                DigPlanEntry(direction = Direction.SOUTH, distance = 2, color = "#d2c081"),
-                DigPlanEntry(direction = Direction.EAST, distance = 2, color = "#59c680"),
-                DigPlanEntry(direction = Direction.SOUTH, distance = 2, color = "#411b91"),
-                DigPlanEntry(direction = Direction.WEST, distance = 5, color = "#8ceee2"),
-                DigPlanEntry(direction = Direction.NORTH, distance = 2, color = "#caa173"),
-                DigPlanEntry(direction = Direction.WEST, distance = 1, color = "#1b58a2"),
-                DigPlanEntry(direction = Direction.NORTH, distance = 2, color = "#caa171"),
-                DigPlanEntry(direction = Direction.EAST, distance = 2, color = "#7807d2"),
-                DigPlanEntry(direction = Direction.NORTH, distance = 3, color = "#a77fa3"),
-                DigPlanEntry(direction = Direction.WEST, distance = 2, color = "#015232"),
-                DigPlanEntry(direction = Direction.NORTH, distance = 2, color = "#7a21e3"),
+                DigPlanEntry(direction = Direction.EAST, distance = 6),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 5),
+                DigPlanEntry(direction = Direction.WEST, distance = 2),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 2),
+                DigPlanEntry(direction = Direction.EAST, distance = 2),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 2),
+                DigPlanEntry(direction = Direction.WEST, distance = 5),
+                DigPlanEntry(direction = Direction.NORTH, distance = 2),
+                DigPlanEntry(direction = Direction.WEST, distance = 1),
+                DigPlanEntry(direction = Direction.NORTH, distance = 2),
+                DigPlanEntry(direction = Direction.EAST, distance = 2),
+                DigPlanEntry(direction = Direction.NORTH, distance = 3),
+                DigPlanEntry(direction = Direction.WEST, distance = 2),
+                DigPlanEntry(direction = Direction.NORTH, distance = 2),
             ),
             digPlan
         )
@@ -55,21 +55,28 @@ internal class Day18Test {
         val trenchEdge = buildTrenchEdge(digPlan)
         assertEquals(
             """
-                #######
-                #.....#
-                ###...#
-                ..#...#
-                ..#...#
-                ###.###
-                #...#..
-                ##..###
-                .#....#
-                .######
+                F.....7
+                .......
+                L.7....
+                .......
+                .......
+                F.J.F.J
+                .......
+                L7..L.7
+                .......
+                .L....J
             """.trimIndent(),
             trenchEdge.to2DString { _, c ->
-                if (c != null) TRENCH else LEVEL_TERRAIN
+                c ?: LEVEL_TERRAIN
             }
         )
+    }
+
+    @Test
+    fun testSumEvenPairDifferences() {
+        assertEquals(102033, sumEvenPairDifferences(
+            setOf(50, 20, 800000, 900000, 102000, 100000)
+        ))
     }
 
     @Test
@@ -88,6 +95,46 @@ internal class Day18Test {
         val trenchEdge = buildTrenchEdge(digPlan)
 //        printTrenchEdge(trenchEdge)
         assertEquals(40131, calculateTrenchVolume(trenchEdge))
+    }
+
+    @Test
+    fun testParseV2() {
+        val digPlan = parseDigPlanV2(sampleInput)
+        assertEquals(
+            listOf(
+                DigPlanEntry(direction = Direction.EAST, distance = 461937),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 56407),
+                DigPlanEntry(direction = Direction.EAST, distance = 356671),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 863240),
+                DigPlanEntry(direction = Direction.EAST, distance = 367720),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 266681),
+                DigPlanEntry(direction = Direction.WEST, distance = 577262),
+                DigPlanEntry(direction = Direction.NORTH, distance = 829975),
+                DigPlanEntry(direction = Direction.WEST, distance = 112010),
+                DigPlanEntry(direction = Direction.SOUTH, distance = 829975),
+                DigPlanEntry(direction = Direction.WEST, distance = 491645),
+                DigPlanEntry(direction = Direction.NORTH, distance = 686074),
+                DigPlanEntry(direction = Direction.WEST, distance = 5411),
+                DigPlanEntry(direction = Direction.NORTH, distance = 500254),
+            ),
+            digPlan
+        )
+    }
+
+    @Test
+    fun testSampleAnswer2() {
+        val digPlan = parseDigPlanV2(sampleInput)
+        val trenchEdge = buildTrenchEdge(digPlan)
+        assertEquals(952_408_144_115, calculateTrenchVolume(trenchEdge))
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer2() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val digPlan = parseDigPlanV2(input)
+        val trenchEdge = buildTrenchEdge(digPlan)
+        assertEquals(104_454_050_898_331, calculateTrenchVolume(trenchEdge))
     }
 
     private fun printTrenchEdge(trenchEdge: Map<Coord, Char>) {
