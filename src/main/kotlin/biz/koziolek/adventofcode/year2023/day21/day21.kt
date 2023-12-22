f2ec526b81e7c055f157cf155aedb71db0b4aaa4
koziolek <github@koziolek.biz>
2023-12-22 18:24:09 +0100

    Work in progress code for 2023-12-21, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day21/day21.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day21/day21.kt
@@ -1,9 +1,6 @@
 package biz.koziolek.adventofcode.year2023.day21
 
-import biz.koziolek.adventofcode.Coord
-import biz.koziolek.adventofcode.findInput
-import biz.koziolek.adventofcode.getAdjacentCoords
-import biz.koziolek.adventofcode.parse2DMap
+import biz.koziolek.adventofcode.*
 
 fun main() {
     val inputFile = findInput(object {})
@@ -20,13 +17,388 @@ const val REACHED_POS = 'O'
 fun parseGardenMap(lines: Iterable<String>): Map<Coord, Char> =
     lines.parse2DMap().toMap()
 
+fun gardenMapToString(map: Map<Coord, Char>, markCoords: Collection<Coord> = emptySet()): String =
+    map.to2DString { coord, char ->
+        if (coord in markCoords) {
+            REACHED_POS
+        } else {
+            char ?: GARDEN_PLOT
+        }
+    }
+
+fun moveStartTo00(map: Map<Coord, Char>): Map<Coord, Char> =
+    when (val start = findStart(map)) {
+        Coord(0, 0) -> map
+        else -> map.mapKeys { (coord, _) -> coord - start }
+    }
+
 fun findStart(map: Map<Coord, Char>) =
     map.entries.single { it.value == START }.key
 
-fun walk(map: Map<Coord, Char>, steps: Int): Set<Coord> =
-    (1..steps).fold(setOf(findStart(map))) { positions, _ ->
-        positions
-            .flatMap { map.getAdjacentCoords(it, includeDiagonal = false) }
-            .filter { map[it] != ROCK }
-            .toSet()
+fun walk(map: Map<Coord, Char>, steps: Int): Set<Coord> {
+    val flooded = flood(map, steps)
+    val remainder = steps % 2
+
+    return flooded.entries
+        .filter { it.value % 2 == remainder }
+        .map { it.key }
+        .toSet()
+}
+
+fun flood(map: Map<Coord, Char>, distance: Int): Map<Coord, Int> {
+    val flooded = mutableMapOf<Coord, Int>()
+    val start = findStart(map)
+    val toCheck = mutableListOf(start to 0)
+    val queued = mutableSetOf(start)
+    var lastSeenDistance = -1
+
+    while (toCheck.isNotEmpty()) {
+        val (coord, walkedDistance) = toCheck.removeFirst()
+
+        if (walkedDistance > distance) {
+            break
+        }
+
+        if (walkedDistance != lastSeenDistance && walkedDistance % 100 == 0) {
+            println("Distance $walkedDistance - flooded ${flooded.size}, queued ${queued.size}, to check ${toCheck.size}")
+            lastSeenDistance = walkedDistance
+        }
+
+        flooded[coord] = walkedDistance
+
+        coord.getAdjacentCoords()
+            .filter { adjCoord -> adjCoord !in queued && map[adjCoord] != ROCK }
+            .forEach {
+                toCheck.add(it to (walkedDistance + 1))
+                queued.add(it)
+            }
     }
+
+    return flooded
+}
+
+fun infiniteWalk(map: Map<Coord, Char>, steps: Int): Long {
+    val startingChunk = Coord(0, 0)
+    val floodedChunks = mutableMapOf<Coord, FloodResult>()
+    val chunksToCheck = LinkedHashMap<Coord, FloodInput>()
+
+    chunksToCheck[startingChunk] = FloodInput(
+        chunk = startingChunk,
+        map = map,
+        start = findStart(map),
+        absoluteStartingDistance = 0,
+    )
+
+    while (chunksToCheck.isNotEmpty()) {
+        val floodInput = chunksToCheck.pollFirstEntry().value
+//        println("Checking chunk ${floodInput.chunk}")
+
+        if (floodedChunks.size % 1000 == 1) {
+            println("Flooded ${floodedChunks.size} chunks - max distance = ${floodedChunks.values.maxOf { it.absoluteMaxDistance }}")
+        }
+
+        val floodResult: FloodResult =
+            floodedChunks.values
+                .firstOrNull { existingResult -> existingResult.input.start == floodInput.start }
+                ?.let { existingResult ->
+                    println("Same as ${existingResult.input.chunk} @ (${existingResult.input.start}) - reusing")
+                    existingResult.reuse(floodInput)
+                }
+                ?: infiniteFlood(floodInput)
+
+        floodedChunks[floodResult.input.chunk] = floodResult
+
+        floodResult.exits
+            .filter { it.toChunk !in floodedChunks && it.toChunk !in chunksToCheck }
+            .map {
+                FloodInput(
+                    chunk = it.toChunk,
+                    map = map,
+                    start = wrapAround(it.mapCoord, map),
+                    absoluteStartingDistance = floodResult.input.absoluteStartingDistance + it.distance,
+                )
+            }
+            .filter {
+//                println("abs distance to ${it.chunk} (starting at ${it.start}): ${it.absoluteStartingDistance}")
+                it.absoluteStartingDistance <= steps
+            }
+            .forEach { chunksToCheck[it.chunk] = it }
+    }
+
+    floodedChunks.to2DStringOfStrings { coord, floodResult ->
+        floodResult?.let { "%9s".format(it.input.start) } ?: "         "
+    }.let { println(it) }
+
+//    var sum = 0L
+//    val chunksToSum = mutableListOf(startingChunk)
+//    val alreadySummed = mutableSetOf<Coord>()
+//    val remainder = steps % 2L
+//
+//    while (chunksToSum.isNotEmpty()) {
+//        val chunk = chunksToSum.removeFirst()
+//        val floodResult = floodedChunks[chunk]!!
+//        val partialSum = countStandingAt(floodResult, steps)
+//
+////        println("Summing chunk $chunk += $partialSum")
+//        sum += partialSum
+//        alreadySummed.add(chunk)
+//
+//        if (floodResult.absoluteMaxDistance < steps) {
+//            floodResult.exits
+//                .map { it.toChunk }
+//                .filter { it !in chunksToSum && it !in alreadySummed }
+//                .forEach { chunksToSum.add(it) }
+//        }
+//    }
+//
+//    return sum
+
+    val center = floodedChunks[startingChunk]!!
+
+    val axes = listOf(
+        Coord(+0, -1),
+        Coord(+1, +0),
+        Coord(+0, +1),
+        Coord(-1, +0),
+    ).map { floodedChunks[it]!! }
+
+    val quadrants = listOf(
+        Coord(+1, -1),
+        Coord(+1, +1),
+        Coord(-1, +1),
+        Coord(-1, -1),
+    ).map { floodedChunks[it]!! }
+
+    val repetitions = 202300L
+    return (countStandingAt(center, steps).toLong()
+            + repetitions * axes.sumOf { countStandingAt(it, steps).toLong() }
+            + (repetitions * (repetitions - 1) / 2L) * quadrants.sumOf { countStandingAt(it, steps).toLong() })
+}
+
+private fun countStandingAt(floodResult: FloodResult, steps: Int): Int {
+    val remainder = steps % 2L
+
+    return if (floodResult.absoluteMaxDistance <= steps) {
+        if (remainder == 0L) {
+            floodResult.absoluteEvenDistances
+        } else {
+            floodResult.absoluteOddDistances
+        }
+    } else {
+        floodResult.relativeDistances
+            .map { it.value + floodResult.input.absoluteStartingDistance }
+            .count { it % 2 == remainder }
+    }
+}
+
+data class FloodInput(
+    val chunk: Coord, // Coordinate of a chunk - map copy moved from original position
+    val map: Map<Coord, Char>,
+    val start: Coord,
+    val absoluteStartingDistance: Long,
+)
+
+data class FloodResult(
+    val input: FloodInput,
+    val relativeDistances: Map<Coord, Int>,
+    val exits: Set<Exit>, // map coord -> chunk coord
+    val maxDistance: Int,
+    val evenDistances: Int,
+    val oddDistances: Int,
+) {
+    val absoluteMaxDistance: Long
+        get() = input.absoluteStartingDistance + maxDistance
+
+    val absoluteEvenDistances: Int
+        get() = if (input.absoluteStartingDistance % 2 == 0L) evenDistances else oddDistances
+
+    val absoluteOddDistances: Int
+        get() = if (input.absoluteStartingDistance % 2 == 0L) oddDistances else evenDistances
+
+    fun reuse(newInput: FloodInput): FloodResult =
+        copy(
+            input = newInput,
+            exits = exits.map { it.reuse(newInput.chunk) }.toSet()
+        )
+}
+
+data class Exit(val mapCoord: Coord, val fromChunk: Coord, val toChunk: Coord, val distance: Int) {
+    fun reuse(newChunk: Coord): Exit {
+        val delta = newChunk - fromChunk
+        return copy(
+            fromChunk = newChunk,
+            toChunk = toChunk + delta,
+        )
+    }
+}
+
+fun infiniteFlood(input: FloodInput): FloodResult {
+    val flooded = mutableMapOf<Coord, Int>()
+    val exits = mutableListOf<Exit>()
+    val toCheck = mutableListOf(input.start to 0)
+    val queued = mutableSetOf(input.start)
+    var lastSeenDistance = -1
+    val xRange = input.map.getHorizontalRange()
+    val yRange = input.map.getVerticalRange()
+
+    while (toCheck.isNotEmpty()) {
+        val (coord, walkedDistance) = toCheck.removeFirst()
+
+        if (walkedDistance != lastSeenDistance && walkedDistance % 100 == 0) {
+//            println("Distance $walkedDistance - flooded ${flooded.size}, queued ${queued.size}, to check ${toCheck.size}")
+            lastSeenDistance = walkedDistance
+        }
+
+        flooded[coord] = walkedDistance
+
+        val adjCoords = coord.getAdjacentCoords()
+        val newDistance = walkedDistance + 1
+
+        for (adjCoord in adjCoords) {
+            var newAdjCoord: Coord? = adjCoord
+
+            val exitDirection = when {
+                adjCoord.x < xRange.first -> Direction.WEST
+                adjCoord.x > xRange.last -> Direction.EAST
+                adjCoord.y < yRange.first -> Direction.NORTH
+                adjCoord.y > xRange.last -> Direction.SOUTH
+                else -> null
+            }
+
+            if (exitDirection != null) {
+//                println("Coord $adjCoord (dist = $newDistance) is outside of the map!")
+                exits.add(Exit(
+                    mapCoord = adjCoord,
+                    fromChunk = input.chunk,
+                    toChunk = input.chunk.move(exitDirection),
+                    distance = newDistance,
+                ))
+                newAdjCoord = null
+            } else {
+                if (adjCoord !in queued && input.map[adjCoord] != ROCK) {
+                    toCheck.add(adjCoord to newDistance)
+                    queued.add(adjCoord)
+                }
+            }
+
+            if (newAdjCoord != null && newAdjCoord !in queued && input.map[newAdjCoord] != ROCK) {
+                toCheck.add(newAdjCoord to newDistance)
+                queued.add(newAdjCoord)
+            }
+        }
+    }
+
+    return FloodResult(
+        input = input,
+        relativeDistances = flooded,
+        exits = exits
+            .groupBy { it.toChunk }
+            .map { exit -> exit.value.minByOrNull { it.distance }!! }
+            .toSet(),
+        maxDistance = flooded.values.max(),
+        evenDistances = flooded.count { it.value % 2 == 0 },
+        oddDistances = flooded.count { it.value % 2 == 1 },
+    )
+}
+
+fun <T> wrapAround(coord: Coord, map: Map<Coord, T>): Coord {
+    val xRange = map.getHorizontalRange()
+    val yRange = map.getVerticalRange()
+
+    return Coord(
+        x = coerceWithLoops(coord.x, xRange),
+        y = coerceWithLoops(coord.y, yRange),
+    )
+}
+
+class InfiniteMap(internal val originalMap: Map<Coord, Char>) : Map<Coord, Char> by originalMap {
+
+    internal val xRange = originalMap.getHorizontalRange()
+    internal val yRange = originalMap.getVerticalRange()
+    internal val xCache = mutableMapOf<Int, Int>()
+    internal val yCache = mutableMapOf<Int, Int>()
+
+    override val keys: Set<Coord>
+        get() = InfiniteKeys(this)
+
+    override fun get(key: Coord): Char? {
+        if (key.x in xRange && key.y in yRange) {
+            return originalMap[key]
+        }
+
+        val originalX = xCache.computeIfAbsent(key.x) { coerce(key.x, xRange) }
+        val originalY = yCache.computeIfAbsent(key.y) { coerce(key.y, yRange) }
+        val originalCoord = Coord(originalX, originalY)
+
+        return when (val originalValue = originalMap[originalCoord]) {
+            START -> null
+            else -> originalValue
+        }
+    }
+}
+
+private class InfiniteKeys(val map: InfiniteMap) : Set<Coord> by map.originalMap.keys {
+
+    override fun contains(element: Coord): Boolean {
+        if (element.x in map.xRange && element.y in map.yRange) {
+            return map.originalMap.contains(element)
+        }
+
+        val originalX = map.xCache.computeIfAbsent(element.x) { coerce(element.x, map.xRange) }
+        val originalY = map.yCache.computeIfAbsent(element.y) { coerce(element.y, map.yRange) }
+        val originalCoord = Coord(originalX, originalY)
+
+        return map.originalMap.contains(originalCoord)
+    }
+}
+
+private val coerceCache = mutableMapOf<Pair<Int, IntRange>, Int>()
+
+internal fun rememberingCoerce(value: Int, range: IntRange): Int {
+    return coerceCache.computeIfAbsent(value to range) { coerce(value, range) }
+}
+
+internal fun coerce(value: Int, range: IntRange): Int {
+    val rangeSize = range.last - range.first + 1
+    val coerced = if (value > range.last) {
+        val tmp = range.first + (value - range.last) % rangeSize
+        if (tmp > range.first) {
+            tmp - 1
+        } else {
+            range.last
+        }
+    } else if (value < range.first) {
+        val tmp = range.last - (range.first - value) % rangeSize
+        if (tmp < range.last) {
+            tmp + 1
+        } else {
+            range.first
+        }
+    } else {
+        value
+    }
+
+    verifyCoercion(value, range, coerced)
+
+    return coerced
+}
+
+
+internal fun coerceWithLoops(value: Int, range: IntRange): Int {
+    var newValue = value
+    val rangeSize = range.last - range.first + 1
+    while (newValue < range.first) {
+        newValue += rangeSize
+    }
+    while (newValue > range.last) {
+        newValue -= rangeSize
+    }
+    return newValue
+}
+
+private fun verifyCoercion(value: Int, range: IntRange, coerced: Int) {
+    val expected = coerceWithLoops(value, range)
+    if (coerced != expected) {
+        println("Coercing mismatch! $value -> $range == $expected != $coerced")
+    }
+}
--- src/test/kotlin/biz/koziolek/adventofcode/year2023/day21/Day21Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2023/day21/Day21Test.kt
@@ -1,11 +1,11 @@
 package biz.koziolek.adventofcode.year2023.day21
 
-import biz.koziolek.adventofcode.findInput
-import biz.koziolek.adventofcode.getHeight
-import biz.koziolek.adventofcode.getWidth
+import biz.koziolek.adventofcode.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @Tag("2023")
 internal class Day21Test {
@@ -35,10 +35,82 @@ internal class Day21Test {
     @Test
     fun testSampleAnswer1() {
         val map = parseGardenMap(sampleInput)
-        assertEquals(2, walk(map, steps = 1).size)
-        assertEquals(4, walk(map, steps = 2).size)
-        assertEquals(6, walk(map, steps = 3).size)
-        assertEquals(16, walk(map, steps = 6).size)
+
+        val walked1 = walk(map, steps = 1)
+        assertEquals(
+            """
+                ...........
+                .....###.#.
+                .###.##..#.
+                ..#.#...#..
+                ....#O#....
+                .##.OS####.
+                .##..#...#.
+                .......##..
+                .##.#.####.
+                .##..##.##.
+                ...........
+            """.trimIndent(),
+            gardenMapToString(map, walked1)
+        )
+        assertEquals(2, walked1.size)
+
+        val walked2 = walk(map, steps = 2)
+        assertEquals(
+            """
+                ...........
+                .....###.#.
+                .###.##..#.
+                ..#.#O..#..
+                ....#.#....
+                .##O.O####.
+                .##.O#...#.
+                .......##..
+                .##.#.####.
+                .##..##.##.
+                ...........
+            """.trimIndent(),
+            gardenMapToString(map, walked2)
+        )
+        assertEquals(4, walked2.size)
+
+        val walked3 = walk(map, steps = 3)
+        assertEquals(
+            """
+                ...........
+                .....###.#.
+                .###.##..#.
+                ..#.#.O.#..
+                ...O#O#....
+                .##.OS####.
+                .##O.#...#.
+                ....O..##..
+                .##.#.####.
+                .##..##.##.
+                ...........
+            """.trimIndent(),
+            gardenMapToString(map, walked3)
+        )
+        assertEquals(6, walked3.size)
+
+        val walked6 = walk(map, steps = 6)
+        assertEquals(
+            """
+                ...........
+                .....###.#.
+                .###.##.O#.
+                .O#O#O.O#..
+                O.O.#.#.O..
+                .##O.O####.
+                .##.O#O..#.
+                .O.O.O.##..
+                .##.#.####.
+                .##O.##.##.
+                ...........
+            """.trimIndent(),
+            gardenMapToString(map, walked6)
+        )
+        assertEquals(16, walked6.size)
     }
 
     @Test
@@ -47,5 +119,240 @@ internal class Day21Test {
         val input = findInput(object {}).bufferedReader().readLines()
         val map = parseGardenMap(input)
         assertEquals(3594, walk(map, steps = 64).size)
+    }
+
+    @Test
+    fun testCoerce() {
+        assertEquals(7, coerce(27, 0..9))
+        assertEquals(7, coerce(-3, 0..9))
+        assertEquals(-7, coerce(3, -9..0))
+        assertEquals(-7, coerce(-27, -9..0))
+        assertEquals(6, coerce(27, -10..10))
+        assertEquals(-6, coerce(-27, -10..10))
+        assertEquals(126, coerce(227, 100..200))
+        assertEquals(184, coerce(83, 100..200))
+        assertEquals(5, coerce(10 + 21 + 21 + 21 + 21 + 10 + 1 + 5, -10..10))
+        assertEquals(-6, coerce(-10 - 21 - 21 - 21 - 10 - 1 - 6, -10..10))
+        assertEquals(0, coerce(-11, 0..10))
+        assertEquals(10, coerce(21, 0..10))
+    }
+
+    @Test
+    fun testInfiniteMap() {
+        val map = InfiniteMap(parseGardenMap(sampleInput))
+        assertEquals(
+            """
+                ...........
+                .....###.#.
+                .###.##..#.
+                ..#.#...#..
+                ....#.#....
+                .##..S####.
+                .##..#...#.
+                .......##..
+                .##.#.####.
+                .##..##.##.
+                ...........
+            """.trimIndent(),
+            gardenMapToString(map)
+        )
+
+        val yRange = -11..21
+        val xRange = -11..21
+        val coords = yRange.flatMap { y -> xRange.map { x -> Coord(x, y) } }
+        val newMap = coords
+            .mapNotNull { coord -> map[coord]?.let { coord to it } }
+            .toMap()
+
+        assertEquals(
+            """
+                .................................
+                .....###.#......###.#......###.#.
+                .###.##..#..###.##..#..###.##..#.
+                ..#.#...#....#.#...#....#.#...#..
+                ....#.#........#.#........#.#....
+                .##...####..##...####..##...####.
+                .##..#...#..##..#...#..##..#...#.
+                .......##.........##.........##..
+                .##.#.####..##.#.####..##.#.####.
+                .##..##.##..##..##.##..##..##.##.
+                .................................
+                .................................
+                .....###.#......###.#......###.#.
+                .###.##..#..###.##..#..###.##..#.
+                ..#.#...#....#.#...#....#.#...#..
+                ....#.#........#.#........#.#....
+                .##...####..##..S####..##...####.
+                .##..#...#..##..#...#..##..#...#.
+                .......##.........##.........##..
+                .##.#.####..##.#.####..##.#.####.
+                .##..##.##..##..##.##..##..##.##.
+                .................................
+                .................................
+                .....###.#......###.#......###.#.
+                .###.##..#..###.##..#..###.##..#.
+                ..#.#...#....#.#...#....#.#...#..
+                ....#.#........#.#........#.#....
+                .##...####..##...####..##...####.
+                .##..#...#..##..#...#..##..#...#.
+                .......##.........##.........##..
+                .##.#.####..##.#.####..##.#.####.
+                .##..##.##..##..##.##..##..##.##.
+                .................................
+            """.trimIndent(),
+            gardenMapToString(newMap)
+        )
+    }
+
+    @Test
+    fun testInfiniteFlood() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val map = moveStartTo00(parseGardenMap(input))
+
+        val mapArea = map.getWidth() * map.getHeight()
+        val allCoords = map.walkSouth().toSet()
+        val rocks = map.filter{ it.value == ROCK }
+        val empty = map.walkSouth().filter { it !in rocks }.toList()
+        println("area: ${map.getWidth()} x ${map.getWidth()} = $mapArea")
+        println("all coords: ${allCoords.size}")
+        println("rocks: ${rocks.size}")
+        println("empty: ${empty.size}")
+        println("empty: ${mapArea - rocks.size}")
+
+        val flooded = infiniteFlood(FloodInput(
+            chunk = Coord(0, 0),
+            map = map,
+            start = findStart(map),
+            absoluteStartingDistance = 0,
+        ))
+        println("Flooded from start: ${flooded.relativeDistances.size}")
+        println("  max distance: ${flooded.relativeDistances.values.max()}")
+
+        println("  not flooded:")
+        val notFlooded = empty.toSet() - flooded.relativeDistances.keys
+        println(notFlooded.joinToString("\n    ", "    "))
+
+        assertEquals(
+            setOf(
+                Exit(mapCoord = Coord(-66, 0), fromChunk = Coord(0, 0), toChunk = Coord(-1, 0), distance = 66),
+                Exit(mapCoord = Coord(0, 66), fromChunk = Coord(0, 0), toChunk = Coord(0, 1), distance = 66),
+                Exit(mapCoord = Coord(66, 0), fromChunk = Coord(0, 0), toChunk = Coord(1, 0), distance = 66),
+                Exit(mapCoord = Coord(0, -66), fromChunk = Coord(0, 0), toChunk = Coord(0, -1), distance = 66),
+            ),
+            flooded.exits
+        )
+
+        println("From north:")
+        val floodedFromNorth = infiniteFlood(FloodInput(Coord(0, 1), map, start = Coord(0, -66), absoluteStartingDistance = 66))
+        println("  max distance: ${floodedFromNorth.relativeDistances.values.max()}")
+
+        println("From east:")
+        val floodedFromEast = infiniteFlood(FloodInput(Coord(-1, 0), map, start = Coord(66, 0), absoluteStartingDistance = 66))
+        println("  max distance: ${floodedFromNorth.relativeDistances.values.max()}")
+
+        println("From south:")
+        val floodedFromSouth = infiniteFlood(FloodInput(Coord(0, -1), map, start = Coord(0, 66), absoluteStartingDistance = 66))
+        println("  max distance: ${floodedFromNorth.relativeDistances.values.max()}")
+
+        println("From west:")
+        val floodedFromWest = infiniteFlood(FloodInput(Coord(1, 0), map, start = Coord(-66, 0), absoluteStartingDistance = 66))
+        println("  max distance: ${floodedFromNorth.relativeDistances.values.max()}")
+
+        println("N == E? ${floodedFromNorth == floodedFromEast}")
+        println("N == S? ${floodedFromNorth == floodedFromSouth}")
+        println("N == W? ${floodedFromNorth == floodedFromWest}")
+
+        val floodFormatter: (Coord, Int?) -> String = { coord, distance ->
+            val str = if (distance != null) {
+                "%4d".format(distance)
+            } else {
+                "    "
+            }
+
+            if (coord.x == 0 || coord.y == 0) {
+                AsciiColor.YELLOW.format(str)
+            } else {
+                str
+            }
+        }
+
+        println(flooded.relativeDistances.to2DStringOfStrings(floodFormatter))
+        println()
+        println(floodedFromNorth.relativeDistances.to2DStringOfStrings(floodFormatter))
+        println()
+        println(floodedFromEast.relativeDistances.to2DStringOfStrings(floodFormatter))
+        println()
+        println(floodedFromSouth.relativeDistances.to2DStringOfStrings(floodFormatter))
+        println()
+        println(floodedFromWest.relativeDistances.to2DStringOfStrings(floodFormatter))
+
+        println(lowestAtEdges(floodedFromNorth))
+        println(floodedFromNorth.exits)
+
+        println(lowestAtEdges(floodedFromEast))
+        println(floodedFromEast.exits)
+
+        println(lowestAtEdges(floodedFromSouth))
+        println(floodedFromSouth.exits)
+
+        println(lowestAtEdges(floodedFromWest))
+        println(floodedFromWest.exits)
+    }
+
+    private fun lowestAtEdges(flooded: FloodResult): Map<Coord, Int> {
+        val (minX, maxX) = flooded.relativeDistances.keys.minAndMaxOrNull { it.x }!!
+        val (minY, maxY) = flooded.relativeDistances.keys.minAndMaxOrNull { it.y }!!
+        val minDistance = flooded.relativeDistances.values.min()
+
+//        return flooded.filter {
+//            it.value == minDistance && (
+//                it.key.x == minX
+//                || it.key.x == maxX
+//                || it.key.y == minY
+//                || it.key.y == maxY
+//            )
+//        }
+
+        return mapOf(
+            flooded.relativeDistances.filter { it.key.x == minX }.minBy { it.value }.toPair(),
+            flooded.relativeDistances.filter { it.key.x == maxX }.minBy { it.value }.toPair(),
+            flooded.relativeDistances.filter { it.key.y == minY }.minBy { it.value }.toPair(),
+            flooded.relativeDistances.filter { it.key.y == maxY }.minBy { it.value }.toPair(),
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+//        "6,16",
+//        "10,50",
+//        "50,1594",
+//        "100,6536",
+        "500,167004",
+//        "1000,668697",
+//        "5000,16733044",
+    )
+    fun testSampleAnswer2(steps: Int, expectedTiles: Int) {
+        val map = parseGardenMap(sampleInput)
+        val infiniteMap = InfiniteMap(map)
+        assertEquals(expectedTiles, walk(infiniteMap, steps).size)
+        assertEquals(expectedTiles, infiniteWalk(map, steps))
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer2() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val map = moveStartTo00(parseGardenMap(input))
+        val infiniteMap = InfiniteMap(map)
+
+        println(walk(infiniteMap, steps = 65).size)
+        println(walk(infiniteMap, steps = 65 + 131).size)
+        println(walk(infiniteMap, steps = 65 + 131 + 131).size)
+//        assertEquals(-1, infiniteWalk(map, steps = 26_501_365))
+        println(26_501_365 / map.getWidth())
+        // 60 501 212 936 -> too low
+        // 604 715 084 751 788 -> too low
+        // 608152828731262
+        assertEquals(-1, infiniteWalk(map, steps = map.getWidth() + 65))
     }
 }
