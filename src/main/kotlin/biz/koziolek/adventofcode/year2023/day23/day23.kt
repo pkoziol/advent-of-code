4e0ca2bfa6fb319603ebb582017fa04cbd8eb000
koziolek <github@koziolek.biz>
2023-12-23 18:40:35 +0100

    Solution for 2023-12-23, part 1

---
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day23/day23.kt
@@ -0,0 +1,86 @@
+package biz.koziolek.adventofcode.year2023.day23
+
+import biz.koziolek.adventofcode.*
+
+fun main() {
+    val inputFile = findInput(object {})
+    val hikingTrails = parseHikingTrails(inputFile.bufferedReader().readLines())
+    println("Longest path has ${findLongestPathLen(hikingTrails)} steps")
+}
+
+const val PATH = '.'
+const val FOREST = '#'
+const val SLOPE_N = '^'
+const val SLOPE_E = '>'
+const val SLOPE_S = 'v'
+const val SLOPE_W = '<'
+
+private val slopeToDirection = mapOf(
+    SLOPE_N to Direction.NORTH,
+    SLOPE_E to Direction.EAST,
+    SLOPE_S to Direction.SOUTH,
+    SLOPE_W to Direction.WEST,
+)
+
+fun parseHikingTrails(lines: Iterable<String>): Map<Coord, Char> =
+    lines.parse2DMap().filter { (_, char) -> char != FOREST }.toMap()
+
+fun findLongestPathLen(hikingTrails: Map<Coord, Char>): Int {
+    val start = hikingTrails.entries.single { it.key.y == 0 && it.value == PATH }.key
+    val distance = hikingTrails.keys.associateWith { Int.MIN_VALUE }.toMutableMap()
+    distance[start] = 0
+
+    val sorted = topologicalSort(hikingTrails, start)
+    val visited = mutableSetOf<Coord>()
+
+    while (sorted.isNotEmpty()) {
+        val coord = sorted.removeFirst()
+        visited.add(coord)
+
+        val newDist = distance[coord]!! + 1
+        for (adjCoord in getAdjacentTiles(hikingTrails, coord).filter { it !in visited }) {
+            if (distance[adjCoord]!! < newDist) {
+                distance[adjCoord] = newDist
+            }
+        }
+    }
+
+    val end = hikingTrails.entries.single { it.key.y == hikingTrails.getHeight() - 1 }.key
+    return distance[end]!!
+}
+
+fun topologicalSort(
+    hikingTrails: Map<Coord, Char>,
+    coord: Coord,
+    visited: MutableSet<Coord> = mutableSetOf(),
+    sorted: MutableList<Coord> = mutableListOf(coord)
+): List<Coord> {
+    if (coord in visited) {
+        return emptyList()
+    }
+
+    visited.add(coord)
+
+    for (adjCoord in getAdjacentTiles(hikingTrails, coord)) {
+        if (adjCoord !in visited) {
+            topologicalSort(hikingTrails, adjCoord, visited, sorted)
+        }
+    }
+
+    sorted.addFirst(coord)
+
+    return sorted
+}
+
+private fun getAdjacentTiles(hikingTrails: Map<Coord, Char>, coord: Coord): List<Coord> =
+    Direction.entries
+        .mapNotNull { direction ->
+            val newCoord = coord.move(direction)
+            val char = hikingTrails[newCoord]
+
+            if (char == PATH || slopeToDirection[char] == direction) {
+                newCoord
+            } else {
+                null
+            }
+        }
