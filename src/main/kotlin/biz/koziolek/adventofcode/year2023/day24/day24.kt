43a318c391844a226ae75e736a25d168e41e0709
koziolek <github@koziolek.biz>
2023-12-24 17:47:05 +0100

    Solution for 2023-12-24, part 2 (Z3)

--- src/main/kotlin/biz/koziolek/adventofcode/year2023/day24/day24.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2023/day24/day24.kt
@@ -1,6 +1,7 @@
 package biz.koziolek.adventofcode.year2023.day24
 
 import biz.koziolek.adventofcode.*
+import java.time.LocalDateTime
 
 fun main() {
     val inputFile = findInput(object {})
@@ -10,7 +11,9 @@ fun main() {
 
 val testArea = LongCoord(200_000_000_000_000, 200_000_000_000_000) to LongCoord(400_000_000_000_000, 400_000_000_000_000)
 
-data class Hail(val position: LongCoord3d, val velocity: Coord3d, val time: Long = 1) {
+data class Hail(val position: LongCoord3d,
+                val velocity: Coord3d,
+                val velocityDelta: Coord3d = Coord3d(0, 0, 0)) {
     override fun toString() = "$position @ $velocity"
 }
 
@@ -32,16 +35,7 @@ fun parseHails(lines: Iterable<String>): List<Hail> =
     }
 
 fun countFutureIntersections(hails: List<Hail>, testArea: Pair<LongCoord, LongCoord>): Int =
-    hails
-        .flatMapIndexed { index1, hail1 ->
-            hails.mapIndexedNotNull { index2, hail2 ->
-                if (index2 > index1) {
-                    hail1 to hail2
-                } else {
-                    null
-                }
-            }
-        }
+    generatePairs(hails)
         .count { (hail1, hail2) ->
             val intersection = findIntersection(hail1, hail2)
             intersection != null
@@ -49,6 +43,17 @@ fun countFutureIntersections(hails: List<Hail>, testArea: Pair<LongCoord, LongCo
                     && intersection.time2 > 0
                     && testArea.contains(intersection.position)
         }
+
+private fun generatePairs(hails: List<Hail>): Sequence<Pair<Hail, Hail>> =
+    sequence {
+        hails.forEachIndexed { index1, hail1 ->
+            hails.forEachIndexed { index2, hail2 ->
+                if (index2 > index1) {
+                    yield(hail1 to hail2)
+                }
+            }
+        }
+    }
 
 fun findIntersection(first: Hail, second: Hail): Intersection? {
     // x1 + vx1 * time = x2 + vx2 * time
@@ -66,19 +71,31 @@ fun findIntersection(first: Hail, second: Hail): Intersection? {
     // a1x + b1 = a2x + b2
     // x = (b1 - b2) / (a2 - a1)
 
+    // x1 + vx1 * time1 = x + vx * time1
+    // y1 + vy1 * time1 = y + vy * time1
+    // z1 + vz1 * time1 = z + vz * time1
+
+    // x2 + vx2 * time2 = x + vx * time2
+    // y2 + vy2 * time2 = y + vy * time2
+    // z2 + vz2 * time2 = z + vz * time2
+
+    // x3 + vx3 * time3 = x + vx * time3
+    // y3 + vy3 * time3 = y + vy * time3
+    // z3 + vz3 * time3 = z + vz * time3
+
     try {
 //        val timeX = (first.position.x.toDouble() - second.position.x) / (second.velocity.x.toDouble() - first.velocity.x)
 //        val timeY = (first.position.y.toDouble() - second.position.y) / (second.velocity.y.toDouble() - first.velocity.y)
-        val a1 = first.velocity.y / first.velocity.x.toDouble()
+        val a1 = (first.velocity.y + first.velocityDelta.y) / (first.velocity.x.toDouble() + first.velocityDelta.x)
         val b1 = first.position.y - a1 * first.position.x
-        val a2 = second.velocity.y / second.velocity.x.toDouble()
+        val a2 = (second.velocity.y + second.velocityDelta.y) / (second.velocity.x.toDouble() + second.velocityDelta.x)
         val b2 = second.position.y - a2 * second.position.x
 
         val x = (b1 - b2) / (a2 - a1)
         val y = a1 * x + b1
 
-        val time1 = (x - first.position.x) / first.velocity.x.toDouble()
-        val time2 = (x - second.position.x) / second.velocity.x.toDouble()
+        val time1 = (x - first.position.x) / (first.velocity.x.toDouble() + first.velocityDelta.x)
+        val time2 = (x - second.position.x) / (second.velocity.x.toDouble() + second.velocityDelta.x)
 
         return if (x.isFinite() && y.isFinite()) {
             Intersection(
@@ -94,5 +111,82 @@ fun findIntersection(first: Hail, second: Hail): Intersection? {
         }
     } catch (e: Exception) {
         throw RuntimeException("Error while intersecting $first and $second", e)
+    }
+}
+
+fun findRockPosition(hails: List<Hail>, x: IntRange, y: IntRange, z: IntRange, logPrefix: String = ""): LongCoord3d {
+    val hailsWithDifferentVectors = generateHails(hails, x, y, z)
+    var index = 0L
+    val points = mutableListOf<LongCoord>()
+
+    for (newHails in hailsWithDifferentVectors) {
+        val point = findCommonIntersectsXY(newHails)
+
+        if (index % 999_000_000 == 0L) {
+            println("${logPrefix}%,12d @ %s".format(index, LocalDateTime.now()))
+        }
+
+        if (point != null) {
+            println("${logPrefix}Found $point at $index for dv = ${newHails.first().velocityDelta}")
+            points.add(point)
+        }
+
+        index++
+    }
+
+    // TODO choose Z that matches
+
+    println("${logPrefix}%,12d @ %s".format(index, LocalDateTime.now()))
+
+    return LongCoord3d(0, 0, 0)
+}
+
+private fun generateHails(hails: List<Hail>, x: IntRange, y: IntRange, z: IntRange): Sequence<List<Hail>> =
+    sequence {
+        for (xx in x) {
+            for (yy in y) {
+                for (zz in z) {
+                    yield(Coord3d(xx, yy, zz))
+                }
+            }
+        }
+    }
+//        .sortedBy { it.distanceTo(Coord3d(0, 0, 0)) }
+        .map { delta ->
+            hails.map {
+                Hail(
+                    position = it.position,
+                    velocity = it.velocity,
+                    velocityDelta = delta
+                )
+            }
+        }
+
+private fun findCommonIntersectsXY(hails: List<Hail>): LongCoord? {
+    var commonIntersection: Intersection? = null
+    val intersections = mutableMapOf<Intersection, Int>()
+
+    for ((hail1, hail2) in generatePairs(hails)) {
+        val intersection = findIntersection(hail1, hail2)
+        if (intersection == null) {
+            continue
+        }
+
+        intersections.compute(intersection) { _, v -> v?.plus(1) ?: 1 }
+
+        if (commonIntersection == null) {
+            commonIntersection = intersection
+        } else {
+            // x = 354954946036320
+            // y = 318916597757112
+            // z = 112745502066835
+            if (commonIntersection.position.distanceTo(intersection.position) > 1) {
+                return null
+            }
+        }
+    }
+
+    return commonIntersection?.position?.let {
+        LongCoord(it.x.toLong(), it.y.toLong())
     }
 }
