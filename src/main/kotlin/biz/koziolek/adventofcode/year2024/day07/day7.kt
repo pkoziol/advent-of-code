e39c8efa365895d44cf5335cee6d2df0b1d5991d
koziolek <github@koziolek.biz>
2024-12-07 11:13:24 +0100

    Solution for 2024-12-07, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2024/day07/day7.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2024/day07/day7.kt
@@ -1,12 +1,13 @@
 package biz.koziolek.adventofcode.year2024.day07
 
 import biz.koziolek.adventofcode.findInput
+import kotlin.math.pow
 
 fun main() {
     val inputFile = findInput(object {})
     val equations = parseCalibrationEquations(inputFile.bufferedReader().readLines())
-    val validEquations = findValidEquations(equations)
-    println("Sum of valid equations: ${validEquations.sumOf { it.value }}")
+    println("Sum of valid equations: ${findValidEquations(equations, operators = listOf('+', '*')).sumOf { it.value }}")
+    println("Sum of valid equations 2: ${findValidEquations(equations, operators = listOf('+', '*', '|')).sumOf { it.value }}")
 }
 
 data class Equation(val value: Long, val factors: List<Long>) {
@@ -16,6 +17,7 @@ data class Equation(val value: Long, val factors: List<Long>) {
             when (operator) {
                 '+' -> acc + factor
                 '*' -> acc * factor
+                '|' -> acc * 10.0.pow(factor.toString().length).toLong() + factor
                 else -> error("Unknown operator: $operator")
             }
         }
@@ -31,17 +33,17 @@ fun parseCalibrationEquations(lines: Iterable<String>): List<Equation> =
         )
     }
 
-fun findValidEquations(equations: List<Equation>): List<Equation> =
+fun findValidEquations(equations: List<Equation>, operators: List<Char>): List<Equation> =
     equations.filter { equation ->
-        val allOperators = generateAllOperators(equation.factors.size - 1)
+        val allOperators = generateAllOperators(equation.factors.size - 1, operators)
         allOperators.any { operators -> equation.evaluate(operators) == equation.value }
     }
 
-fun generateAllOperators(n: Int): List<List<Char>> =
+fun generateAllOperators(n: Int, availableOperators: List<Char>): List<List<Char>> =
     if (n == 1) {
-        listOf(listOf('+'), listOf('*'))
+        availableOperators.map { listOf(it) }
     } else {
-        generateAllOperators(n - 1).flatMap { operators ->
-            listOf('+', '*').map { operators + it }
+        generateAllOperators(n - 1, availableOperators).flatMap { operators ->
+            availableOperators.map { operators + it }
         }
     }
