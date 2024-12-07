c9a46c04e3bfbda6f16d8fc890fecce0c29bb5af
koziolek <github@koziolek.biz>
2024-12-07 11:13:24 +0100

    Solution for 2023-12-07, part 2

--- src/test/kotlin/biz/koziolek/adventofcode/year2024/day07/Day7Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2024/day07/Day7Test.kt
@@ -31,12 +31,15 @@ internal class Day7Test {
         assertEquals(123, Equation(123, listOf(100, 23)).evaluate(listOf('+')))
         assertEquals(24, Equation(24, listOf(2, 3, 4)).evaluate(listOf('*', '*')))
         assertEquals(20, Equation(20, listOf(2, 3, 4)).evaluate(listOf('+', '*')))
+        assertEquals(156, Equation(156, listOf(15, 6)).evaluate(listOf('|')))
+        assertEquals(7290, Equation(7290, listOf(6, 8, 6, 15)).evaluate(listOf('*', '|', '*')))
+        assertEquals(192, Equation(192, listOf(17, 8, 14)).evaluate(listOf('|', '+')))
     }
 
     @Test
     fun testSampleAnswer1() {
         val equations = parseCalibrationEquations(sampleInput)
-        val validEquations = findValidEquations(equations)
+        val validEquations = findValidEquations(equations, operators = listOf('+', '*'))
         assertEquals(3749, validEquations.sumOf { it.value })
     }
 
@@ -45,7 +48,23 @@ internal class Day7Test {
     fun testAnswer1() {
         val input = findInput(object {}).bufferedReader().readLines()
         val equations = parseCalibrationEquations(input)
-        val validEquations = findValidEquations(equations)
+        val validEquations = findValidEquations(equations, operators = listOf('+', '*'))
         assertEquals(20281182715321, validEquations.sumOf { it.value })
+    }
+
+    @Test
+    fun testSampleAnswer2() {
+        val equations = parseCalibrationEquations(sampleInput)
+        val validEquations = findValidEquations(equations, operators = listOf('+', '*', '|'))
+        assertEquals(11387, validEquations.sumOf { it.value })
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer2() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val equations = parseCalibrationEquations(input)
+        val validEquations = findValidEquations(equations, operators = listOf('+', '*', '|'))
+        assertEquals(159490400628354, validEquations.sumOf { it.value })
     }
 }
