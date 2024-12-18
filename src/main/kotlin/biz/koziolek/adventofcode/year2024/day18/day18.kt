18b50d8238c71a7176dd2ee5ee5601d2f1f73703
koziolek <github@koziolek.biz>
2024-12-18 06:59:04 +0100

    Solution for 2024-12-18, part 1

---
+++ src/main/kotlin/biz/koziolek/adventofcode/year2024/day18/day18.kt
@@ -0,0 +1,72 @@
+package biz.koziolek.adventofcode.year2024.day18
+
+import biz.koziolek.adventofcode.*
+
+fun main() {
+    val inputFile = findInput(object {})
+    val bytes = parseFallingBytes(inputFile.bufferedReader().readLines())
+    val memory = buildMemory(bytes.take(1024))
+    val path = findExitPath(memory)
+    println("Steps to exit after 1kB: ${path.size - 1}")
+}
+
+const val EMPTY = '.'
+const val CORRUPTED = '#'
+const val PATH = 'O'
+
+fun parseFallingBytes(lines: Iterable<String>): List<Coord> =
+    lines.map { Coord.fromString(it) }
+
+fun buildMemory(bytes: List<Coord>, size: Int = 70): Map<Coord, Char> =
+    buildMap {
+        for (x in 0..size) {
+            for (y in 0..size) {
+                put(Coord(x, y), EMPTY)
+            }
+        }
+        for (byte in bytes) {
+            put(byte, CORRUPTED)
+        }
+    }
+
+fun findExitPath(memory: Map<Coord, Char>): List<Coord> {
+    val start = Coord(0, 0)
+    val exit = Coord(memory.getWidth() - 1, memory.getHeight() - 1)
+
+    val graph = buildGraph {
+        memory.keys
+            .filter { memory[it] == EMPTY }
+            .forEach { coord ->
+                val node1 = coord.toGraphNode()
+
+                memory.getAdjacentCoords(coord, includeDiagonal = false)
+                    .filter { memory[it] == EMPTY }
+                    .forEach { adjCoord ->
+                        add(
+                            BiDirectionalGraphEdge(
+                                node1 = node1,
+                                node2 = adjCoord.toGraphNode(),
+                            )
+                        )
+                    }
+            }
+    }
+
+    val path = graph.findShortestPath(start.toGraphNode(), exit.toGraphNode())
+    return path.map { it.coord }
+}
+
+fun drawMemory(memory: Map<Coord, Char>): String = drawPath(memory, emptyList())
+
+fun drawPath(memory: Map<Coord, Char>, path: List<Coord>): String =
+    memory.to2DStringOfStrings { coord, c ->
+        if (coord in path) {
+            AsciiColor.WHITE.format(PATH)
+        } else if (c == CORRUPTED) {
+            AsciiColor.RED.format(CORRUPTED)
+        } else if (c == EMPTY) {
+            AsciiColor.BRIGHT_BLACK.format(EMPTY)
+        } else {
+            throw IllegalArgumentException("Unknown memory value $c at $coord")
+        }
+    }
---
+++ src/test/kotlin/biz/koziolek/adventofcode/year2024/day18/Day18Test.kt
@@ -0,0 +1,83 @@
+package biz.koziolek.adventofcode.year2024.day18
+
+import biz.koziolek.adventofcode.AsciiColor
+import biz.koziolek.adventofcode.findInput
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag("2024")
+internal class Day18Test {
+
+    private val sampleInput = """
+            5,4
+            4,2
+            4,5
+            3,0
+            2,1
+            6,3
+            2,4
+            1,5
+            0,6
+            3,3
+            2,6
+            5,1
+            1,2
+            5,5
+            2,5
+            6,5
+            1,4
+            0,4
+            6,4
+            1,1
+            6,1
+            1,0
+            0,5
+            1,6
+            2,0
+        """.trimIndent().split("\n")
+
+    @Test
+    fun testParseFallingBytes() {
+        val bytes = parseFallingBytes(sampleInput)
+        assertEquals(25, bytes.size)
+    }
+
+    @Test
+    fun testSampleAnswer1() {
+        val bytes = parseFallingBytes(sampleInput)
+
+        val memory = buildMemory(bytes.take(12), size = 6)
+        assertEquals("""
+            ...#...
+            ..#..#.
+            ....#..
+            ...#..#
+            ..#..#.
+            .#..#..
+            #.#....
+        """.trimIndent(), AsciiColor.cleanUp(drawMemory(memory)))
+
+        val path = findExitPath(memory)
+        assertEquals("""
+            OO.#OOO
+            .O#OO#O
+            .OOO#OO
+            ...#OO#
+            ..#OO#.
+            .#.O#..
+            #.#OOOO
+        """.trimIndent(), AsciiColor.cleanUp(drawPath(memory, path)))
+        assertEquals(22, path.size - 1)
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer1() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val bytes = parseFallingBytes(input)
+        val memory = buildMemory(bytes.take(1024))
+        val path = findExitPath(memory)
+        assertEquals(338, path.size - 1)
+    }
+}
