707036c84160d94b41466be484dd606b34053a16
koziolek <github@koziolek.biz>
2024-12-22 09:05:19 +0100

    Solution for 2024-12-22, part 2

--- src/main/kotlin/biz/koziolek/adventofcode/year2024/day22/day22.kt
+++ src/main/kotlin/biz/koziolek/adventofcode/year2024/day22/day22.kt
@@ -5,8 +5,14 @@ import biz.koziolek.adventofcode.findInput
 fun main() {
     val inputFile = findInput(object {})
     val initialSecrets = parseInitialSecrets(inputFile.bufferedReader().readLines())
+
     val twoThousandths = getSecretNumbers(initialSecrets, n = 2000)
     println("Sum of 2000th secret numbers: ${sumSecrets(twoThousandths)}")
+
+    val prices = initialSecrets.map { getPrices(it).take(2001).toList() }
+    val bestSeq = findBestSequence(prices)
+    val total = sellAll(prices, bestSeq)
+    println("Most bananas we can get is $total for sequence $bestSeq")
 }
 
 fun parseInitialSecrets(lines: Iterable<String>): List<Int> =
@@ -47,3 +53,91 @@ fun prune(secret: Long): Long =
 
 fun sumSecrets(secrets: List<Int>): Long =
     secrets.sumOf { it.toLong() }
+
+fun getPrices(initialSecret: Int): Sequence<Int> =
+    sequence {
+        yield(initialSecret % 10)
+        generateSecretNumbers(initialSecret).forEach {
+            yield(it % 10)
+        }
+    }
+
+fun sellAll(pricesList: List<List<Int>>, sellSequence: SellSequence): Int =
+    pricesList.sumOf { prices ->
+        sell(prices, sellSequence) ?: 0
+    }
+
+fun sell(prices: List<Int>, sellSequence: SellSequence): Int? {
+    var matched = 0
+
+    val changes = prices
+        .zipWithNext()
+        .map { (prev, current) ->
+            (current - prev) to current
+        }
+
+    for ((change, price) in changes) {
+        if (change == sellSequence[matched]) {
+            matched++
+            if (matched == 4) {
+                return price
+            }
+        } else if (change == sellSequence[0]) {
+            matched = 1
+        } else {
+            matched = 0
+        }
+    }
+
+    return null
+}
+
+fun findBestSequence(pricesList: List<List<Int>>): SellSequence {
+    val totalPerSequence = mutableMapOf<SellSequence, Int>()
+
+    for (prices in pricesList) {
+        val changes = prices
+            .zipWithNext()
+            .map { (prev, current) ->
+                (current - prev) to current
+            }
+
+        val first3Changes = SellSequence(
+            0,
+            changes[0].first,
+            changes[1].first,
+            changes[2].first,
+        )
+
+        val heardSequences = mutableSetOf<SellSequence>()
+        changes.drop(3).fold(first3Changes) { sellSequence, (change, price) ->
+            val nextSellSequence = sellSequence.next(change)
+            if (nextSellSequence !in heardSequences) {
+                totalPerSequence.compute(nextSellSequence) { _, v ->
+                    (v ?: 0) + price
+                }
+            }
+            heardSequences.add(nextSellSequence)
+            nextSellSequence
+        }
+    }
+
+    return totalPerSequence.maxBy { it.value }.key
+}
+
+data class SellSequence(val a: Int, val b: Int, val c: Int, val d: Int) {
+    fun next(newD: Int) =
+        SellSequence(b, c, d, newD)
+
+    operator fun get(index: Int): Int? =
+        when (index) {
+            0 -> a
+            1 -> b
+            2 -> c
+            3 -> d
+            else -> null
+        }
+
+    override fun toString(): String =
+        "($a, $b, $c, $d)"
+}
--- src/test/kotlin/biz/koziolek/adventofcode/year2024/day22/Day22Test.kt
+++ src/test/kotlin/biz/koziolek/adventofcode/year2024/day22/Day22Test.kt
@@ -15,6 +15,13 @@ internal class Day22Test {
             2024
         """.trimIndent().split("\n")
 
+    private val sampleInput2 = """
+            1
+            2
+            3
+            2024
+        """.trimIndent().split("\n")
+
     @Test
     fun testParseInitialSecrets() {
         val initialSecrets = parseInitialSecrets(sampleInput)
@@ -65,5 +72,56 @@ internal class Day22Test {
         val initialSecrets = parseInitialSecrets(input)
         val twoThousandths = getSecretNumbers(initialSecrets, n = 2000)
         assertEquals(20411980517, sumSecrets(twoThousandths))
+    }
+
+    @Test
+    fun testGetPrices() {
+        assertEquals(
+            listOf(3, 0, 6, 5, 4, 4, 6, 4, 4, 2),
+            getPrices(123).take(10).toList()
+        )
+    }
+
+    @Test
+    fun testSell() {
+        val prices = listOf(3, 0, 6, 5, 4, 4, 6, 4, 4, 2)
+        assertEquals(6, sell(prices, SellSequence(-1, -1, 0, 2)))
+        assertEquals(4, sell(prices, SellSequence(-1, 0, 2, -2)))
+        assertEquals(null, sell(prices, SellSequence(6, 0, 2, 0)))
+        assertEquals(4, sell(prices, SellSequence(-3, 6, -1, -1)))
+        assertEquals(2, sell(prices, SellSequence(2, -2, 0, -2)))
+    }
+
+    @Test
+    fun testSampleAnswer2Small() {
+        val initialSecrets = listOf(123)
+        val prices = initialSecrets.map { getPrices(it).take(10).toList() }
+        val bestSeq = findBestSequence(prices)
+        assertEquals(SellSequence(-1, -1, 0, 2), bestSeq)
+        val total = sellAll(prices, bestSeq)
+        assertEquals(6, total)
+    }
+
+    @Test
+    fun testSampleAnswer2Big() {
+        val initialSecrets = parseInitialSecrets(sampleInput2)
+        val prices = initialSecrets.map { getPrices(it).take(2001).toList() }
+        val bestSeq = findBestSequence(prices)
+        val total = sellAll(prices, bestSeq)
+        assertEquals(
+            SellSequence(-2, 1, -1, 3) to 23,
+            bestSeq to total
+        )
+    }
+
+    @Test
+    @Tag("answer")
+    fun testAnswer2() {
+        val input = findInput(object {}).bufferedReader().readLines()
+        val initialSecrets = parseInitialSecrets(input)
+        val prices = initialSecrets.map { getPrices(it).take(2001).toList() }
+        val bestSeq = findBestSequence(prices)
+        val total = sellAll(prices, bestSeq)
+        assertEquals(2362, total)
     }
 }
